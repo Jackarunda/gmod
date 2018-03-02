f32679ceda43c2c4848123b4e06adfe40d9f5547
Lavaman007 <brenthesom@gmail.com>
2018-03-02 12:30:57 -0600

    Update shared.lua

--- lua/entities/ent_jack_turretammobox_shot/shared.lua
+++ lua/entities/ent_jack_turretammobox_shot/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "12GA Ammo Box"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
+ENT.Category			= "JMod - Turrets"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
