a17cb87b990abf6bbc2788d08a63f2e61c32bfb9
Lavaman007 <brenthesom@gmail.com>
2018-03-02 11:46:09 -0600

    Update shared.lua

--- lua/entities/ent_jack_fgironslug_large/shared.lua
+++ lua/entities/ent_jack_fgironslug_large/shared.lua
@@ -3,7 +3,6 @@ ENT.Base="base_anim"
 ENT.PrintName="Large Iron Slug"
 ENT.Author="Jackarunda"
 ENT.Information=""
-ENT.Category="Jackarunda's FunGuns"
-
+ENT.Category="JMod - Weapons"
 ENT.Spawnable=true
 ENT.AdminSpawnable=true
