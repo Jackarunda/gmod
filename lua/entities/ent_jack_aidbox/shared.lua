96cb5b928f5b3767897bc5300139b1f29c2be3a6
Lavaman007 <brenthesom@gmail.com>
2018-03-02 14:13:12 -0600

    Update shared.lua

--- lua/entities/ent_jack_aidbox/shared.lua
+++ lua/entities/ent_jack_aidbox/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "J.I. Aid Package"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
+ENT.Category			= "JMod - Technical"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
