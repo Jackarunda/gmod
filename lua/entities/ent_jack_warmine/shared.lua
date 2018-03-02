3e382b43da0c8d8fbc6f6d30b9c94ea23060089a
Lavaman007 <brenthesom@gmail.com>
2018-03-02 12:34:23 -0600

    Update shared.lua

--- lua/entities/ent_jack_warmine/shared.lua
+++ lua/entities/ent_jack_warmine/shared.lua
@@ -1,10 +1,7 @@
-
-
 ENT.Type 			= "anim"
 ENT.PrintName		= "War Mine"
 ENT.Author			= "Jackarunda"
 ENT.Category			= "JMod - Mines"
 ENT.Information         = "beep beep boom"
-
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
