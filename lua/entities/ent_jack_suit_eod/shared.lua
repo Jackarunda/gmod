b14e690153385438bbecfcc76dab95b643b6443d
Lavaman007 <brenthesom@gmail.com>
2018-03-02 12:04:21 -0600

    Update shared.lua

--- lua/entities/ent_jack_suit_eod/shared.lua
+++ lua/entities/ent_jack_suit_eod/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "EOD Suit"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
+ENT.Category			= "JMod - Armor"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
