cfb1a293b859e9f50d597c73a2ff2f121f0cc24a
Lavaman007 <brenthesom@gmail.com>
2018-03-02 14:47:20 -0600

    Update shared.lua

--- lua/entities/ent_jack_terminal/shared.lua
+++ lua/entities/ent_jack_terminal/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "Sentry Terminal"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "JMod - Other"
+ENT.Category			= "JMod - Turrets"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
