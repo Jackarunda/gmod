79cdc603f79ffa9162504d84db8ba17f999b8145
Lavaman007 <brenthesom@gmail.com>
2018-03-02 11:37:00 -0600

    Update shared.lua

--- lua/entities/ent_jack_bodyarmor_helm_st/shared.lua
+++ lua/entities/ent_jack_bodyarmor_helm_st/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type 			= "anim"
-ENT.PrintName		= "Steel Helmet"
+ENT.PrintName		= "JMod - Armor"
 ENT.Author			= "Jackarunda"
 ENT.Category			= "J.I. Defense Solutions"
 ENT.Information         = "BLOOIE-SCHLANG"
