4365d0cb0984b609dd000513b8e7cd6f63d057f0
Lavaman007 <brenthesom@gmail.com>
2018-03-02 12:37:17 -0600

    Update shared.lua

--- lua/entities/ent_jack_aidfood/shared.lua
+++ lua/entities/ent_jack_aidfood/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "Box of Food"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
+ENT.Category			= "JMod - Other"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
