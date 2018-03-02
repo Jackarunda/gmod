4a4dceb1997d50565fb0819b2442b44936aa0948
Lavaman007 <brenthesom@gmail.com>
2018-03-02 14:45:29 -0600

    Update shared.lua

--- lua/entities/ent_jack_turretammobox_base/shared.lua
+++ lua/entities/ent_jack_turretammobox_base/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "Ammo Box"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "JMod - Turrets"
+ENT.Category			= "JMod - Technical"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
