086f904996ee0bd5d21db0b8017cab9536c44e69
Lavaman007 <brenthesom@gmail.com>
2018-03-02 14:17:24 -0600

    Update shared.lua

--- lua/entities/ent_jack_turretmissile/shared.lua
+++ lua/entities/ent_jack_turretmissile/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "Heatseeking Missile"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "Jackarunda's Explosives"
+ENT.Category			= "JMod - Technical"
 ENT.Information         = "goes zoom outta the missile launcher and blows shit up"
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
