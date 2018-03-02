a17e7f318a8bfc1242c48c12ba9100e747dfde6c
Lavaman007 <brenthesom@gmail.com>
2018-03-02 11:59:30 -0600

    Update shared.lua

--- lua/entities/ent_jack_slam/shared.lua
+++ lua/entities/ent_jack_slam/shared.lua
@@ -1,10 +1,7 @@
-
-
 ENT.Type 			= "anim"
 ENT.PrintName		= "S.L.A.M. Mine"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "J.I. Defense Solutions"
+ENT.Category			= "JMod - Weapons"
 ENT.Information         = "boing BOOM"
--- I've never personally met or seen a lesbian IRL that wasn't ugly
 ENT.Spawnable			= true
 ENT.AdminSpawnable		= true
