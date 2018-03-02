192a18f5af1eb5d0e143519ba1eb6cc44edba0d7
Lavaman007 <brenthesom@gmail.com>
2018-03-02 14:15:47 -0600

    Update shared.lua

--- lua/entities/ent_jack_mischl2ammobox/shared.lua
+++ lua/entities/ent_jack_mischl2ammobox/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 			= "anim"
 ENT.PrintName		= "Misc. HL2 Ammo Box"
 ENT.Author			= "Jackarunda"
-ENT.Category			= "JI Defense Solutions"
+ENT.Category			= "JMod - Technical"
 ENT.Information         = "BLOOIE-SCHLANG"
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
