8bdd668646fe69feeefb682763be026f473607bc
Lavaman007 <brenthesom@gmail.com>
2018-03-02 11:29:26 -0600

    Update shared.lua

--- lua/entities/ent_jack_fgc_energy_rite/shared.lua
+++ lua/entities/ent_jack_fgc_energy_rite/shared.lua
@@ -3,7 +3,7 @@ ENT.Base="base_anim"
 ENT.PrintName="Atomic Battery"
 ENT.Author="Jackarunda"
 ENT.Information=""
-ENT.Category="Jackarunda's FunGuns"
+ENT.Category="JMod - Weapons"
 
 ENT.Spawnable=true
 ENT.AdminSpawnable=true
