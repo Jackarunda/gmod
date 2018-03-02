a6aa5a0f14d0e4df5a371f989ab9ecbc4e3b317e
Lavaman007 <brenthesom@gmail.com>
2018-03-02 14:12:45 -0600

    Update shared.lua

--- lua/entities/ent_jack_40mmgrenade/shared.lua
+++ lua/entities/ent_jack_40mmgrenade/shared.lua
@@ -1,7 +1,7 @@
 AddCSLuaFile()
 ENT.Type="anim"
 ENT.Purpose="Base for all Jackie entities"
-ENT.Category="Jackarunda's Explosives"
+ENT.Category="JMod - Technical"
 ENT.Spawnable=false
 ENT.AdminOnly=false
 ENT.PrintName="40mm Grenade"
