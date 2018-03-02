fd4b833fe42a2d8f31b829769d654d8058c8152f
Lavaman007 <brenthesom@gmail.com>
2018-03-02 11:54:09 -0600

    Update shared.lua

--- lua/entities/ent_jack_fgweaponbox_zeta/shared.lua
+++ lua/entities/ent_jack_fgweaponbox_zeta/shared.lua
@@ -3,7 +3,6 @@ ENT.Base="base_anim"
 ENT.PrintName="HandGun Zeta"
 ENT.Author="Jackarunda"
 ENT.Information=""
-ENT.Category="Jackarunda's FunGuns"
-
+ENT.Category="JMod - Weapons"
 ENT.Spawnable=true
 ENT.AdminSpawnable=true
