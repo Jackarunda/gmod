0c33cafb6d82039b945de6ee40f6ae512d9373eb
TheOnly8Z <13565172+TheOnly8Z@users.noreply.github.com>
2019-11-28 03:58:11 -0500

    Impact nade (No. 69)

---
+++ lua/entities/ent_jack_gmod_ezimpactnade.lua
@@ -0,0 +1,135 @@
+-- Jackarunda 2019
+AddCSLuaFile()
+ENT.Type="anim"
+ENT.Author="Jackarunda, TheOnly8Z"
+ENT.Category="JMod - EZ"
+ENT.Information="glhfggwpezpznore"
+ENT.PrintName="EZ Impact Grenade"
+ENT.Spawnable=true
+ENT.AdminSpawnable=true
+---
+ENT.JModPreferredCarryAngles=Angle(0,-140,0)
+---
+local STATE_BROKEN,STATE_OFF,STATE_PRIMED,STATE_ARMING,STATE_ARMED=-1,0,1,2,3
+function ENT:SetupDataTables()
+	self:NetworkVar("Int",0,"State")
+end
+---
+if(SERVER)then
+	function ENT:SpawnFunction(ply,tr)
+		local SpawnPos=tr.HitPos+tr.HitNormal*20
+		local ent=ents.Create(self.ClassName)
+		ent:SetAngles(Angle(0,0,0))
+		ent:SetPos(SpawnPos)
+		ent.Owner=ply
+		ent:Spawn()
+		ent:Activate()
+		--local effectdata=EffectData()
+		--effectdata:SetEntity(ent)
+		--util.Effect("propspawn",effectdata)
+		return ent
+	end
+	function ENT:Initialize()
+		self.Entity:SetModel("models/codww2/equipment/no, 69 hand grenade.mdl")
+		self.Entity:PhysicsInit(SOLID_VPHYSICS)
+		self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+		self.Entity:SetSolid(SOLID_VPHYSICS)
+		self.Entity:DrawShadow(true)
+		self.Entity:SetUseType(ONOFF_USE)
+		---
+		timer.Simple(.01,function()
+			self:GetPhysicsObject():SetMass(15)
+			self:GetPhysicsObject():Wake()
+		end)
+		---
+		self:SetState(STATE_OFF)
+	end
+	function ENT:PhysicsCollide(data,physobj)
+		if(data.DeltaTime>0.2 and data.Speed>30)then
+			self.Entity:EmitSound("Grenade.ImpactHard")
+			if((self:GetState()==STATE_ARMED)and(data.Speed>200))then
+				self:Detonate()
+			end
+		end
+	end
+	function ENT:OnTakeDamage(dmginfo)
+		if(dmginfo:GetInflictor()==self)then return end
+		self.Entity:TakePhysicsDamage(dmginfo)
+		local Dmg=dmginfo:GetDamage()
+		if(Dmg>=4)then
+			local Pos,State,DetChance=self:GetPos(),self:GetState(),0
+			if(dmginfo:IsDamageType(DMG_BLAST))then DetChance=DetChance+Dmg/150 end
+			if(math.Rand(0,1)<DetChance)then self:Detonate() end
+			if((math.random(1,10)==3)and not(State==STATE_BROKEN))then
+				sound.Play("Metal_Box.Break",Pos)
+				self:SetState(STATE_BROKEN)
+				SafeRemoveEntityDelayed(self,10)
+			end
+		end
+	end
+	function ENT:Use(activator,activatorAgain,onOff)
+		local Dude=activator or activatorAgain
+		self.Owner=Dude
+		local Time=CurTime()
+		if(tobool(onOff))then
+			local State=self:GetState()
+			if(State<0)then return end
+			local Alt=Dude:KeyDown(IN_WALK)
+			if(State==STATE_OFF and Alt)then
+				self:SetState(STATE_PRIMED)
+				local Spewn=ents.Create("ent_jack_spoon")
+				Spewn:SetPos(self:GetPos())
+				Spewn:Spawn()
+				Spewn:SetModel("models/codww2/equipment/no, 69 hand grenade cap.mdl")
+				Spewn:Activate()
+				Spewn:GetPhysicsObject():SetVelocity(self:GetPhysicsObject():GetVelocity()+VectorRand()*750)
+				self:EmitSound("physics/plastic/plastic_box_impact_hard1.wav",60,100)
+				self:SetBodygroup(2,1)
+			end
+			JMod_Hint(activator,"grenade")
+			JMod_ThrowablePickup(Dude,self)
+		end
+	end
+	function ENT:Think()
+		local State,Time=self:GetState(),CurTime()
+		if(State==STATE_PRIMED)then
+			if not(self:IsPlayerHolding())then
+				self:SetState(STATE_ARMING)
+				timer.Simple(.2,function()
+					if(IsValid(self))then self:SetState(STATE_ARMED) end
+				end)
+			end
+			self:NextThink(Time+.1)
+			return true
+		end
+	end
+	function ENT:Detonate()
+		if(self.Exploded)then return end
+		self.Exploded=true
+		local SelfPos=self:GetPos()
+		local Sploom=ents.Create("env_explosion")
+		Sploom:SetPos(SelfPos)
+		Sploom:SetOwner(self.Owner or game.GetWorld())
+		Sploom:SetKeyValue("iMagnitude",math.random(10,20))
+		Sploom:Spawn()
+		Sploom:Activate()
+		Sploom:Fire("explode","",0)
+		self:EmitSound("snd_jack_fragsplodeclose.wav",90,100)
+		local Blam=EffectData()
+		Blam:SetOrigin(SelfPos)
+		Blam:SetScale(0.5)
+		util.Effect("eff_jack_plastisplosion",Blam,true,true)
+		util.ScreenShake(SelfPos,20,20,1,1000)
+		util.BlastDamage(self,self.Owner or game.GetWorld(),SelfPos,200,200)
+		self:Remove()
+	end
+elseif(CLIENT)then
+	function ENT:Initialize()
+		--
+	end
+	local GlowSprite=Material("sprites/mat_jack_basicglow")
+	function ENT:Draw()
+		self:DrawModel()
+	end
+	language.Add("ent_jack_gmod_eznade_impact","EZminiNade-Impact")
+end
