e20b27922c037aee89819fabe5d04e2259e093e4
TheOnly8Z <13565172+TheOnly8Z@users.noreply.github.com>
2019-12-08 00:18:10 -0500

    New explosives in default craft menu

--- lua/autorun/jmod_shared.lua
+++ lua/autorun/jmod_shared.lua
@@ -20,7 +20,7 @@ end
 function JMod_InitGlobalConfig()
 	local NewConfig={
 		Author="Jackarunda",
-		Version=14,
+		Version=15,
 		Note="radio packages must have all lower-case names",
 		Hints=true,
 		SentryPerformanceMult=1,
@@ -150,22 +150,32 @@ function JMod_InitGlobalConfig()
 			["HL2 Buggy"]={"FUNC spawnHL2buggy",{parts=500,power=50,advparts=10,fuel=300,ammo=600},2}
 		},
 		Recipes={
-			["EZ Landmine"]={"ent_jack_gmod_ezlandmine",{parts=10,explosives=5}},
 			["EZ Medkit"]={"ent_jack_gmod_ezmedkit",{parts=20,medsupplies=200}},
-			["EZ Fumigator"]={"ent_jack_gmod_ezfumigator",{parts=30,gas=100,chemicals=50}},
-			["EZ Fougasse Mine"]={"ent_jack_gmod_ezfougasse",{parts=20,fuel=100,explosives=5}},
-			["EZ Detpack"]={"ent_jack_gmod_ezdetpack",{parts=5,explosives=20}},
-			["EZ Time Bomb"]={"ent_jack_gmod_eztimebomb",{parts=30,explosives=180}},
 			["EZ Build Kit"]={"ent_jack_gmod_ezbuildkit",{parts=100,advparts=20,gas=50,power=50}},
 			["EZ Ammo"]={"ent_jack_gmod_ezammo",{parts=30,chemicals=30,explosives=5}},
 			["EZ Explosives"]={"ent_jack_gmod_ezexplosives",{parts=5,chemicals=150}},
 			["EZ Medical Supplies"]={"ent_jack_gmod_ezmedsupplies",{parts=50,chemicals=100,advparts=20}},
-			["EZ Fragmentation Grenade"]={"ent_jack_gmod_ezfragnade",{parts=10,explosives=5}},
+			
+			["EZ Landmine"]={"ent_jack_gmod_ezlandmine",{parts=10,explosives=5}},
+			["EZ Bounding Mine"]={"ent_jack_gmod_ezboundingmine",{parts=20,explosives=5}},
+			["EZ Fumigator"]={"ent_jack_gmod_ezfumigator",{parts=30,gas=100,chemicals=50}},
+			["EZ Fougasse Mine"]={"ent_jack_gmod_ezfougasse",{parts=20,fuel=100,explosives=5}},
+			["EZ Detpack"]={"ent_jack_gmod_ezdetpack",{parts=5,explosives=20}},
+			["EZ Time Bomb"]={"ent_jack_gmod_eztimebomb",{parts=30,explosives=180}},
+			["EZ SLAM"]={"ent_jack_gmod_ezslam",{parts=20,explosives=15}},
+
 			["EZ Mini Impact Grenade"]={"ent_jack_gmod_eznade_impact",{parts=5,explosives=3}},
 			["EZ Mini Proximity Grenade"]={"ent_jack_gmod_eznade_proximity",{parts=5,explosives=3}},
 			["EZ Mini Timed Grenade"]={"ent_jack_gmod_eznade_timed",{parts=5,explosives=3}},
 			["EZ Mini Remote Grenade"]={"ent_jack_gmod_eznade_remote",{parts=5,explosives=3}},
-			["EZ SLAM"]={"ent_jack_gmod_ezslam",{parts=20,explosives=15}}
+			
+			["EZ Frag Grenade"]={"ent_jack_gmod_ezfragnade",{parts=10,explosives=5}},
+			["EZ Gas Grenade"]={"ent_jack_gmod_ezgasnade",{parts=5,gas=20,chemicals=15}},
+			["EZ Impact Grenade"]={"ent_jack_gmod_ezimpactnade",{parts=5,explosives=10}},
+			["EZ Incendiary Grenade"]={"ent_jack_gmod_ezfirenade",{parts=5,explosives=5,fuel=30}},
+			["EZ Satchel Charge"]={"ent_jack_gmod_ezsatchelcharge",{parts=10,explosives=30}},
+			["EZ Stick Grenade"]={"ent_jack_gmod_ezsticknade",{parts=15,explosives=5}},
+			["EZ Sticky Bomb"]={"ent_jack_gmod_ezstickynade",{parts=10,explosives=10,chemicals=10}},
 		}
 	}
 	local FileContents=file.Read("jmod_config.txt")
