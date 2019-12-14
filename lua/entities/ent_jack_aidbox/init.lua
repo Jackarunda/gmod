3e312519f15908f5f1a5e98084f89b6ff0ec62b5
Jackarunda <jackarunda@live.com>
2019-12-13 21:01:14 -0700

    fuck valve, JK

--- lua/entities/ent_jack_aidbox/init.lua
+++ lua/entities/ent_jack_aidbox/init.lua
@@ -108,7 +108,7 @@ function ENT:Use(activator,caller)
 			end
 			if(Ent)then
 				Ent.Owner=activator
-				if(Ent.SetOwner)then Ent:SetOwner(activator) end
+				--if(Ent.SetOwner)then Ent:SetOwner(activator) end -- fuck you valve fuck you gaben fuck you garry
 			end
 		end
 	end
