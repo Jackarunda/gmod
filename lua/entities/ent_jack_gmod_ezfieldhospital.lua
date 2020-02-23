efb890c15b4e146d3e213ee13df33f0d94e0315a
TheOnly8Z <888888zombies@gmail.com>
2020-02-23 18:57:31 -0500

    JMod_CanFieldHospitalStart hook

--- lua/entities/ent_jack_gmod_ezfieldhospital.lua
+++ lua/entities/ent_jack_gmod_ezfieldhospital.lua
@@ -255,6 +255,10 @@ if(SERVER)then
 	end
 	function ENT:TryStartOperation()
 		if not(IsValid(self.Patient))then return end
+
+		local override = hook.Run("JMod_CanFieldHospitalStart", self, self.Patient)
+		if override == false then return end
+
 		local Helf,Max=self.Patient:Health(),self.Patient:GetMaxHealth()
 		if(Helf>=Max)then return end -- you're not hurt lol gtfo
 		if(self:GetSupplies()<=0)then return end
