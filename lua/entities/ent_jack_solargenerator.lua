16bc78eb5e998e3fbb61eb8a18eecf538ab03109
Ruben Medic Diedericks <ruben.diedericks@gmail.com>
2020-08-31 09:53:05 +0200

    Solar Generator Damage Functions

--- lua/entities/ent_jack_solargenerator.lua
+++ lua/entities/ent_jack_solargenerator.lua
@@ -4,7 +4,7 @@ ENT.Type 			= "anim"
 ENT.PrintName		= "EZ Solar Generator"
 ENT.Author			= "Jackarunda, TheOnly8Z"
 ENT.Category			= "JMod - EZ Misc."
-ENT.Information         = ""
+ENT.Information		 = ""
 ENT.Spawnable			= true
 
 ENT.MaxPower = 100
@@ -20,180 +20,209 @@ local STATE_OFF, STATE_BROKEN, STATE_ON = 0, -1, 1
 
 if SERVER then
 
-    function ENT:SpawnFunction(ply,tr,ClassName)
-        local ent=ents.Create(ClassName)
-        ent:SetPos(tr.HitPos + tr.HitNormal*4)
-        ent:SetAngles(ply:GetAngles())
-        ent:Spawn()
-        ent:Activate()
-        local effectdata=EffectData()
-        effectdata:SetEntity(ent)
-        util.Effect("propspawn",effectdata)
-        return ent
-    end
+	function ENT:SpawnFunction(ply,tr,ClassName)
+		local ent=ents.Create(ClassName)
+		ent:SetPos(tr.HitPos + tr.HitNormal*4)
+		ent:SetAngles(ply:GetAngles())
+		ent:Spawn()
+		ent:Activate()
+		local effectdata=EffectData()
+		effectdata:SetEntity(ent)
+		util.Effect("propspawn",effectdata)
+		return ent
+	end
 
-    function ENT:Initialize()
-        self:SetModel("models/jmod/solar_generator.mdl")
-        self:PhysicsInit(SOLID_VPHYSICS)
-        self:SetMoveType(MOVETYPE_VPHYSICS)	
-        self:SetSolid(SOLID_VPHYSICS)
-        self:DrawShadow(true)
-        local phys = self:GetPhysicsObject()
-        if phys:IsValid() then
-            phys:Wake()
-            phys:SetMass(500)
-        end
-        self:SetUseType(SIMPLE_USE)
-        
-        self:SetPower(0)
-        self:SetState(STATE_OFF)
-        self.NextUse = 0
-    end
-    
+	function ENT:Initialize()
+		self:SetModel("models/jmod/solar_generator.mdl")
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetMoveType(MOVETYPE_VPHYSICS)	
+		self:SetSolid(SOLID_VPHYSICS)
+		self:DrawShadow(true)
+		local phys = self:GetPhysicsObject()
+		if phys:IsValid() then
+			phys:Wake()
+			phys:SetMass(500)
+		end
+		self:SetUseType(SIMPLE_USE)
 
-    function ENT:Use(activator,caller)
+		self:SetPower(0)
+		self:SetState(STATE_OFF)
+		self.NextUse = 0
+		self.MaxDurability=100
+		self.Durability=1
+	end
 
-        if(activator:IsPlayer())then
-            
-            if self.NextUse > CurTime() then return end
-            self.NextUse = CurTime() + 0.5
-            
-            if (self:GetState() == STATE_OFF) then
-                if self:GetPower() >= JMod_EZbatterySize then
-                    local canplace = self:ProducePower()
-                    if not canplace and (self.NextWhine or 0) < CurTime() then
-                        self:EmitSound("items/suitchargeno1.wav",70,100)
-                        self.NextWhine = CurTime() + 5
-                    end
-                else
-                    self:Start()
-                end
-            elseif (self:GetState() == STATE_ON) then
-                self:ShutOff()
-            end
-        end
-        
-    end
-    
-    function ENT:CheckSky()
-    
-        local tr = util.TraceLine({
-            start = self:GetPos() + self:GetUp() * 64,
-            endpos = self:GetPos() + Vector(0,0,999999),
-            filter = self
-        })
-        if not tr.HitSky then return 0 end
-        
-        -- TODO fancy sky magic math
-        return 1
-    end
-    
-    function ENT:SpawnEffect(pos)
-        local effectdata=EffectData()
-        effectdata:SetOrigin(pos)
-        effectdata:SetNormal((VectorRand()+Vector(0,0,1)):GetNormalized())
-        effectdata:SetMagnitude(math.Rand(5,10))
-        effectdata:SetScale(math.Rand(.5,1.5))
-        effectdata:SetRadius(math.Rand(2,4))
-        util.Effect("Sparks", effectdata)
-        self:EmitSound("items/suitchargeok1.wav", 80, 120)
-    end
-    
-    function ENT:ProducePower()
+	function ENT:Break(dmginfo)
+		if(self:GetState()==STATE_BROKEN)then return end
+		self:EmitSound("snd_jack_turretbreak.wav",70,math.random(80,120))
+		for i=1,10 do self:DamageSpark() end
+		self.Durability=0
+		self:SetState(STATE_BROKEN)
+		local Force=dmginfo:GetDamageForce()
+	end
+	function ENT:OnTakeDamage(dmginfo)
+		if(self)then
+			self:TakePhysicsDamage(dmginfo)
+			self.Durability=self.Durability-dmginfo:GetDamage()/3
+			if(self.Durability<=0)then self:Break(dmginfo) end
+			if(self.Durability<=-100)then self:Destroy(dmginfo) end
+		end
+	end
 
-        local amt = math.min(math.floor(self:GetPower()), JMod_EZbatterySize)
-        if amt <= 0 then return end
-        
-        local pos = self:GetPos() + self:GetForward() * -48 + self:GetUp() * 24
-        
-        for _, ent in pairs(ents.FindInSphere(pos, 100)) do
-            print(ent, ent.GetResourceType and ent:GetResourceType())
-            if ent:GetClass() == "ent_jack_gmod_ezcrate" and (ent:GetResourceType() == "generic"
-                    or (ent:GetResourceType() == "power" and ent:GetResource() + amt <= ent.MaxResource)) then
-                    
-                if ent:GetResourceType() == "generic" then
-                    ent:ApplySupplyType("power")
-                end
-                    
-                ent:SetResource(math.min(ent:GetResource() + amt, ent.MaxResource))
-                self:SetPower(self:GetPower() - amt)
-                
-                self:SpawnEffect(pos)
-                
-                return true
-            end
-        end
-        
-        -- Ensure the battery isn't spawning in other stuff
-        local tr = util.TraceHull({
-            start = pos,
-            entpos = pos,
-            filter = self,
-            mins = Vector(-16, -16, -16),
-            maxs = Vector(16, 16, 16)
-        })
-        if tr.Hit then return false end
-        
-        self:SetPower(self:GetPower() - amt)
-        
-        local battery = ents.Create(self.BatteryEnt)
-        battery:SetPos(pos)
-        battery:SetAngles(self:GetAngles() + Angle(0, -90, 90))
-        battery:Spawn()
-        battery:Activate()
-        battery:SetResource(amt)
-        battery.NextLoad=CurTime()+1
-        
-        local effectdata=EffectData()
-        effectdata:SetEntity(battery)
-        util.Effect("propspawn",effectdata)
-        
-        self:SpawnEffect(pos)
-        
-        return true
-    end
-    
-    function ENT:Start()
-        if self:CheckSky() > 0 then
-            self:EmitSound("buttons/button1.wav", 60, 80)
-            self:SetState(STATE_ON)
-            self.NextUse = CurTime() + 1
-        else
-            self:EmitSound("buttons/button2.wav", 60, 100)
-        end
-    end
+	function ENT:DamageSpark()
+		local effectdata=EffectData()
+		effectdata:SetOrigin(self:GetPos()+self:GetUp()*30+VectorRand()*math.random(0,10))
+		effectdata:SetNormal(VectorRand())
+		effectdata:SetMagnitude(math.Rand(2,4)) --amount and shoot hardness
+		effectdata:SetScale(math.Rand(.5,1.5)) --length of strands
+		effectdata:SetRadius(math.Rand(2,4)) --thickness of strands
+		util.Effect("Sparks",effectdata,true,true)
+		self:EmitSound("snd_jack_turretfizzle.wav",70,100)
+	end
 
-    function ENT:ShutOff()
-        self:EmitSound("buttons/button18.wav", 60, 80)
-        self:SetState(STATE_OFF)
-        self.NextUse = CurTime() + 1
-    end
-    
-    function ENT:Think()
-        
-        if self:GetState() == STATE_ON then
-            
-            local eff = self:CheckSky()
-            if eff <= 0 or self:WaterLevel() >= 2 then
-                self:ShutOff()
-                return
-            elseif self:GetPower() < self.MaxPower then
-                local rate = 0.1 * eff
-                self:SetPower(math.min(self:GetPower() + rate, self.MaxPower))
-                if self:GetPower() >= JMod_EZbatterySize then
-                    local canplace = self:ProducePower()
-                    if not canplace and (self.NextWhine or 0) < CurTime() then
-                        -- Blocked, let's make some noise to complain
-                        self:EmitSound("items/suitchargeno1.wav",70,100)
-                        self.NextWhine = CurTime() + 10
-                    end
-                end
-            end
+	function ENT:Use(activator,caller)
 
-            self:NextThink(CurTime() + 1)
-            return true
-            
-        end
-        
-    end
+		if(activator:IsPlayer())then
+			
+			if self.NextUse > CurTime() then return end
+			self.NextUse = CurTime() + 0.5
+			
+			if (self:GetState() == STATE_OFF) then
+				if self:GetPower() >= JMod_EZbatterySize then
+					local canplace = self:ProducePower()
+					if not canplace and (self.NextWhine or 0) < CurTime() then
+						self:EmitSound("items/suitchargeno1.wav",70,100)
+						self.NextWhine = CurTime() + 5
+					end
+				else
+					self:Start()
+				end
+			elseif (self:GetState() == STATE_ON) then
+				self:ShutOff()
+			end
+		end
+		
+	end
+		
+	function ENT:CheckSky()
+		
+		local tr = util.TraceLine({
+			start = self:GetPos() + self:GetUp() * 64,
+			endpos = self:GetPos() + Vector(0,0,999999),
+			filter = self
+		})
+		if not tr.HitSky then return 0 end
+		
+		-- TODO fancy sky magic math
+		return 1
+	end
+		
+	function ENT:SpawnEffect(pos)
+		local effectdata=EffectData()
+		effectdata:SetOrigin(pos)
+		effectdata:SetNormal((VectorRand()+Vector(0,0,1)):GetNormalized())
+		effectdata:SetMagnitude(math.Rand(5,10))
+		effectdata:SetScale(math.Rand(.5,1.5))
+		effectdata:SetRadius(math.Rand(2,4))
+		util.Effect("Sparks", effectdata)
+		self:EmitSound("items/suitchargeok1.wav", 80, 120)
+	end
+		
+	function ENT:ProducePower()
+		if self:GetState() == STATE_BROKEN then self:ShutOff() return false end
+		local amt = math.min(math.floor(self:GetPower()), JMod_EZbatterySize)
+		if amt <= 0 then return end
+
+		local pos = self:GetPos() + self:GetForward() * -48 + self:GetUp() * 24
+
+		for _, ent in pairs(ents.FindInSphere(pos, 100)) do
+			print(ent, ent.GetResourceType and ent:GetResourceType())
+			if ent:GetClass() == "ent_jack_gmod_ezcrate" and (ent:GetResourceType() == "generic"
+					or (ent:GetResourceType() == "power" and ent:GetResource() + amt <= ent.MaxResource)) then
+
+				if ent:GetResourceType() == "generic" then
+					ent:ApplySupplyType("power")
+				end
+
+				ent:SetResource(math.min(ent:GetResource() + amt, ent.MaxResource))
+				self:SetPower(self:GetPower() - amt)
+
+				self:SpawnEffect(pos)
+
+				return true
+			end
+		end
+
+		-- Ensure the battery isn't spawning in other stuff
+		local tr = util.TraceHull({
+			start = pos,
+			entpos = pos,
+			filter = self,
+			mins = Vector(-16, -16, -16),
+			maxs = Vector(16, 16, 16)
+		})
+		if tr.Hit then return false end
+		
+		self:SetPower(self:GetPower() - amt)
+		
+		local battery = ents.Create(self.BatteryEnt)
+		battery:SetPos(pos)
+		battery:SetAngles(self:GetAngles() + Angle(0, -90, 90))
+		battery:Spawn()
+		battery:Activate()
+		battery:SetResource(amt)
+		battery.NextLoad=CurTime()+1
+		
+		local effectdata=EffectData()
+		effectdata:SetEntity(battery)
+		util.Effect("propspawn",effectdata)
+		
+		self:SpawnEffect(pos)
+		
+		return true
+	end
+		
+	function ENT:Start()
+		if self:CheckSky() > 0 then
+			self:EmitSound("buttons/button1.wav", 60, 80)
+			self:SetState(STATE_ON)
+			self.NextUse = CurTime() + 1
+		else
+			self:EmitSound("buttons/button2.wav", 60, 100)
+		end
+	end
+
+	function ENT:ShutOff()
+		self:EmitSound("buttons/button18.wav", 60, 80)
+		self:SetState(STATE_OFF)
+		self.NextUse = CurTime() + 1
+	end
+		
+	function ENT:Think()
+		
+		if self:GetState() == STATE_ON then
+			
+			local eff = self:CheckSky()
+			if eff <= 0 or self:WaterLevel() >= 2 or self:GetState() == STATE_BROKEN then
+				self:ShutOff()
+				return
+			elseif self:GetPower() < self.MaxPower then
+				local rate = 0.1 * eff
+				self:SetPower(math.min(self:GetPower() + rate, self.MaxPower))
+				if self:GetPower() >= JMod_EZbatterySize then
+					local canplace = self:ProducePower()
+					if not canplace and (self.NextWhine or 0) < CurTime() then
+						-- Blocked, let's make some noise to complain
+						self:EmitSound("items/suitchargeno1.wav",70,100)
+						self.NextWhine = CurTime() + 10
+					end
+				end
+			end
+
+			self:NextThink(CurTime() + 1)
+			return true
+			
+		end
+		
+	end
 end
