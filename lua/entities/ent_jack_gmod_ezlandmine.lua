f816a969566f08b9e5ced41cf7413426817936aa
klen-list <anton.burkov.02@mail.ru>
2021-05-01 16:40:35 +0300

    Added ENT.JModIsMine for any checks

--- lua/entities/ent_jack_gmod_ezlandmine.lua
+++ lua/entities/ent_jack_gmod_ezlandmine.lua
@@ -9,6 +9,7 @@ ENT.NoSitAllowed=true
 ENT.Spawnable=true
 ENT.AdminSpawnable=true
 ---
+ENT.JModIsMine = true
 ENT.JModEZstorable=true
 ENT.JModPreferredCarryAngles=Angle(0,0,0)
 ENT.BlacklistedNPCs={"bullseye_strider_focus","npc_turret_floor","npc_turret_ceiling","npc_turret_ground"}
