b9783ca60cd3ca89743570287a0cd48190b2e828
Wgairborne <31876279+Wgairborne@users.noreply.github.com>
2021-07-21 19:25:42 -0700

    the stuff
    it do

--- lua/entities/ent_jack_gmod_ezmunitions.lua
+++ lua/entities/ent_jack_gmod_ezmunitions.lua
@@ -7,7 +7,7 @@ ENT.Spawnable=true
 ENT.AdminSpawnable=true
 ---
 ENT.EZsupplies=JMod.EZ_RESOURCE_TYPES.MUNITIONS
-ENT.JModPreferredCarryAngles=Angle(0,0,0)
+ENT.JModPreferredCarryAngles=Angle(0,180,0)
 ENT.MaxResource=JMod.EZammoBoxSize
 ENT.Model="models/Items/BoxJRounds.mdl"
 ENT.Material="models/mat_jack_gmod_ezmunitionbox"
@@ -37,29 +37,11 @@ if(SERVER)then
 		JMod.GiveAmmo(ply,self)
 	end
 elseif(CLIENT)then
-	local TxtCol=Color(255,240,150,80)
 	function ENT:Draw()
-		local Ang,Pos=self:GetAngles(),self:GetPos()
-		local Closeness=LocalPlayer():GetFOV()*(EyePos():Distance(Pos))
-		local DetailDraw=Closeness<18000 -- cutoff point is 200 units when the fov is 90 degrees
 		self:DrawModel()
-		if(DetailDraw)then
-			local Up,Right,Forward,Ammo=Ang:Up(),Ang:Right(),Ang:Forward(),tostring(self:GetResource())
-			Ang:RotateAroundAxis(Ang:Right(),90)
-			Ang:RotateAroundAxis(Ang:Up(),-90)
-			cam.Start3D2D(Pos+Up*21-Right*.6-Forward*7.2,Ang,.05)
-			draw.SimpleText("JACKARUNDA INDUSTRIES","JMod-Stencil",0,0,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
-			draw.SimpleText("EZ MUNITIONS","JMod-Stencil",0,50,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
-			draw.SimpleText(Ammo.." COUNT","JMod-Stencil",0,100,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
-			cam.End3D2D()
-			---
-			Ang:RotateAroundAxis(Ang:Right(),180)
-			cam.Start3D2D(Pos+Up*21-Right*.6+Forward*7.2,Ang,.05)
-			draw.SimpleText("JACKARUNDA INDUSTRIES","JMod-Stencil",0,0,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
-			draw.SimpleText("EZ MUNITIONS","JMod-Stencil",0,50,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
-			draw.SimpleText(Ammo.." COUNT","JMod-Stencil",0,100,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
-			cam.End3D2D()
-		end
+		JMod.HoloGraphicDisplay(self,Vector(-0.7,7.4,14),Angle(-90,0,90),.055,300,function()
+			JMod.StandardResourceDisplay(JMod.EZ_RESOURCE_TYPES.MUNITIONS,self:GetResource(),nil,0,0,200,false)
+		end)
 	end
 	language.Add(ENT.ClassName,ENT.PrintName)
 end
--- lua/entities/ent_jack_gmod_ezpropellant.lua
+++ lua/entities/ent_jack_gmod_ezpropellant.lua
@@ -7,7 +7,7 @@ ENT.Spawnable=true
 ENT.AdminSpawnable=true
 ---
 ENT.EZsupplies=JMod.EZ_RESOURCE_TYPES.PROPELLANT
-ENT.JModPreferredCarryAngles=Angle(0,90,0)
+ENT.JModPreferredCarryAngles=Angle(0,-90,0)
 ENT.MaxResource=JMod.EZbasicResourceBoxSize
 ENT.Model="models/props_lab/jar01b.mdl"
 ENT.Material="models/entities/mat_jack_powderbottle"
@@ -55,8 +55,11 @@ if(SERVER)then
 		--
 	end
 elseif(CLIENT)then
-	local TxtCol=Color(255,255,255,80)
 	function ENT:Draw()
+		self:DrawModel()
+		JMod.HoloGraphicDisplay(self,Vector(6.8,0,-1),Angle(0,0,90),.035,300,function()
+			JMod.StandardResourceDisplay(JMod.EZ_RESOURCE_TYPES.PROPELLANT,self:GetResource(),nil,0,0,200,true)
+	--[[
 		local Ang,Pos=self:GetAngles(),self:GetPos()
 		local Closeness=LocalPlayer():GetFOV()*(EyePos():Distance(Pos))
 		local DetailDraw=Closeness<18000 -- cutoff point is 200 units when the fov is 90 degrees
@@ -71,7 +74,8 @@ elseif(CLIENT)then
 			draw.SimpleText("EZ PROPELLANT","JMod-Stencil",0,50,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
 			draw.SimpleText(Ammo.." UNITS","JMod-Stencil",0,100,TxtCol,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP)
 			cam.End3D2D()
-		end
+			--]]
+		end)
 	end
 	language.Add(ENT.ClassName,ENT.PrintName)
 end
