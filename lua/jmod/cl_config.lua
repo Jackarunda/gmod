2231ef7631af6c08cd8a8a6023d5796ec7bd74bc
Jackarunda <jackarunda@live.com>
2021-09-20 23:09:12 -0700

    saving work

--- lua/jmod/cl_config.lua
+++ lua/jmod/cl_config.lua
@@ -4,24 +4,24 @@ local function InsertEmptyFileConfig(IP)
 		WorkbenchFavs = {},
 		BuildKitFavs = {},
 		AidRadioFavs = {}
-	}		
+	}
 end
 local function PlayerConnectClient()
 	local SinglePlayer = game.SinglePlayer()
 	local IP = game.GetIPAddress()
 	if SinglePlayer then IP = "SinglePlayer" end
 	local filename = "jmod_cl_config.txt"
-	local Here = file.Exists(filename)
+	local Here = file.Exists(filename,"DATA")
 	if Here then
 		local Contents = file.Read(filename)
         JMod.ClientConfig=util.JSONToTable(Contents)
     else
 		JMod.ClientConfig={}
-		JMod.ClientConfig=InsertEmptyFileConfig(IP)
+		InsertEmptyFileConfig(IP)
 		file.Write(filename, util.TableToJSON(JMod.ClientConfig))
 	end
 	if not JMod.ClientConfig[IP] then 
-		JMod.ClientConfig=InsertEmptyFileConfig(IP)
+		InsertEmptyFileConfig(IP)
 		file.Write(filename, util.TableToJSON(JMod.ClientConfig))
 	end
 	print("JMOD: client config file loaded")
@@ -29,18 +29,10 @@ end
 -- That way you are overriding the default hook.
 -- You can use hook.Add to make more functions get called when this event occurs.
 hook.Add( "InitPostEntity", "PlayerConnect", function()
-	print( "Created a JMOD clientside configuration file." )
+	PlayerConnectClient()
 end )
-
-
-
-
-
-
-
-
 concommand.Add("jmod_cl_reloadconfig",function(ply)
 	if CLIENT then
-	JMod.InitClientConfig(ply)
+		PlayerConnectClient()
 	end
 end)
