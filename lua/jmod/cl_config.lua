fb24fbf842512db53d71c314772ecea3c28e3aea
Jackarunda <jackarunda@live.com>
2021-09-30 12:08:36 -0700

    yeehaw

--- lua/jmod/cl_config.lua
+++ lua/jmod/cl_config.lua
@@ -1,9 +1,9 @@
 JMod.ClientConfig = JMod.ClientConfig or {}
-local function InsertEmptyFileConfig(IP)
-	JMod.ClientConfig[IP] = {
-		WorkbenchFavs = {""},
-		BuildKitFavs = {""},
-		AidRadioFavs = {""}
+local function InsertEmptyConfig(Tab,IP)
+	Tab[IP] = {
+		WorkbenchFavs = {},
+		BuildKitFavs = {},
+		AidRadioFavs = {}
 	}
 end
 local function PlayerConnectClient()
@@ -12,23 +12,35 @@ local function PlayerConnectClient()
 	if SinglePlayer then IP = "SinglePlayer" end
 	local filename = "jmod_cl_config.txt"
 	local Here = file.Exists(filename,"DATA")
+	local AllConfigs={}
 	if Here then
 		local Contents = file.Read(filename)
-        JMod.ClientConfig=util.JSONToTable(Contents)
+        AllConfigs=util.JSONToTable(Contents)
     else
-		JMod.ClientConfig={}
-		InsertEmptyFileConfig(IP)
-		file.Write(filename, util.TableToJSON(JMod.ClientConfig, true))
+		InsertEmptyConfig(AllConfigs,IP)
+		file.Write(filename, util.TableToJSON(AllConfigs, true))
 	end
-	if not JMod.ClientConfig[IP] then 
-		InsertEmptyFileConfig(IP)
-		file.Write(filename, util.TableToJSON(JMod.ClientConfig, true))
+	if not AllConfigs[IP] then 
+		InsertEmptyConfig(AllConfigs,IP)
+		file.Write(filename, util.TableToJSON(AllConfigs, true))
 	end
+	JMod.ClientConfig=AllConfigs[IP]
 	print("JMOD: client config file loaded")
-	PrintTable(JMod.ClientConfig)
 end
--- That way you are overriding the default hook.
--- You can use hook.Add to make more functions get called when this event occurs.
+function JMod.SaveClientConfig()
+	local SinglePlayer = game.SinglePlayer()
+	local IP = game.GetIPAddress()
+	if SinglePlayer then IP = "SinglePlayer" end
+	local filename = "jmod_cl_config.txt"
+	local Here = file.Exists(filename,"DATA")
+	local AllConfigs={}
+	if Here then
+		local Contents = file.Read(filename)
+        AllConfigs=util.JSONToTable(Contents)
+    end
+	AllConfigs[IP]=JMod.ClientConfig
+	file.Write(filename,util.TableToJSON(AllConfigs,true))
+end
 hook.Add( "InitPostEntity", "PlayerConnect", function()
 	PlayerConnectClient()
 end )
