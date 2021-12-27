18ee0a134a5eece0295128356814ee41681a5fdf
Jackarunda <jackarunda@protonmail.com>
2021-12-27 13:27:23 -0700

    saving work

--- lua/jmod/sh_locales.lua
+++ lua/jmod/sh_locales.lua
@@ -3,6 +3,43 @@ for i,f in pairs(file.Find("jmod/locales/*.lua", "LUA"))do
     AddCSLuaFile("jmod/locales/" .. f)
     include("jmod/locales/" .. f)
 end
-function JMod.Lang(key)
-    -- todo
+if(CLIENT)then
+    function JMod.Lang(key)
+        
+    end
 end
+
+--[[
+en
+bg
+cs
+da
+de
+el
+en-PT
+es-ES
+et
+fi
+fr
+he
+hr
+hu
+it
+ja
+ko
+lt
+nl
+no
+pl
+pt-BR
+pt-PT
+ru
+sk
+sv-SE
+th
+tr
+uk
+vi
+zh-CN
+zh-TW
+--]]
