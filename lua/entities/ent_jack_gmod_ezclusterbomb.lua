9522638d00229316871f0d30687835809dcee74a
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2021-12-31 16:20:12 -0800

    Update ent_jack_gmod_ezclusterbomb.lua
    Making bomb output desc more concise.

--- lua/entities/ent_jack_gmod_ezclusterbomb.lua
+++ lua/entities/ent_jack_gmod_ezclusterbomb.lua
@@ -49,11 +49,11 @@ if(SERVER)then
 		self.FreefallTicks=0
 		if istable(WireLib) then
 			self.Inputs = WireLib.CreateInputs(self, {"Detonate", "Arm"}, {"This will directly detonate the bomb", "Arms bomb when > 0"})
-			self.Outputs = WireLib.CreateOutputs(self, {"State"}, {"1 is armed \n 0 is not \n -1 is broken"})
+			self.Outputs = WireLib.CreateOutputs(self, {"State"}, {"-1 broken \n 0 off \n 1 armed"})
 		end
 	end
 	function ENT:TriggerInput(iname, value)
-		if(iname == "Detonate") and (self:GetState() == STATE_ARMED) and (value ~= 0) then
+		if(iname == "Detonate") and (value ~= 0) then
 			self:Detonate()
 		elseif iname == "Arm" and value > 0 then
 			self:SetState(STATE_ARMED)
