415d9a1aff1f713087a557ca304d26bd6e0bebac
Kekky Shmekky <kekboxofficial@gmail.com>
2022-02-04 15:58:24 -0500

    Update sv_concommand.lua

--- lua/jmod/sv_concommand.lua
+++ lua/jmod/sv_concommand.lua
@@ -4,29 +4,39 @@ concommand.Add("jmod_friends",function(ply)
 	net.Start("JMod_Friends")
 	net.WriteBit(false)
 	net.WriteTable(ply.JModFriends or {})
-	net.Send(ply)	
-end)
+	net.Send(ply)
+end, nil, "Opens a menu for you to modify your friend list.")
 
 concommand.Add("jmod_reloadconfig",function(ply)
 	if((IsValid(ply))and not(ply:IsSuperAdmin()))then return end
 	JMod.InitGlobalConfig()
-end)
+end, nil, "Refreshes your server config file located in garrysmod/data/jmod_config.txt")
 
 concommand.Add("jmod_resetconfig",function(ply)
 	if((IsValid(ply))and not(ply:IsSuperAdmin()))then return end
 	JMod.InitGlobalConfig(true)
-end)
+end, nil, "Refreshes your server config file located in garrysmod/data/jmod_config.txt")
+
+concommand.Add("jmod_debug_checksalvage",function(ply,cmd,args)
+        if not(IsValid(ply) and ply:IsSuperAdmin())then return end
+        local Ent=ply:GetEyeTrace().Entity
+        if(Ent)then
+            local Yield,Msg=JMod.GetSalvageYield(Ent)
+            print(Msg)
+            PrintTable(Yield)
+        end
+    end, nil, "Prints out the potential resource yield from the object you're looking at.")
 
 concommand.Add("jmod_debug_killme",function(ply)
 	if not(IsValid(ply))then return end
 	if not(GetConVar("sv_cheats"):GetBool())then return end
 	ply.EZkillme=true
 	print("good luck")
-end)
+end, nil, "Makes all your entities hate you.")
 
 concommand.Add("jmod_ez_trigger",function(ply, help)
 	JMod.EZ_Remote_Trigger(ply)
-end)
+end, nil, "Triggers any EZ bombs/mini-nades you have armed.")
 
 concommand.Add("jmod_insta_upgrade",function(ply)
 	if not(IsValid(ply))then return end
@@ -35,17 +45,17 @@ concommand.Add("jmod_insta_upgrade",function(ply)
 	if((IsValid(Ent))and(Ent.EZupgradable))then
 		Ent:Upgrade()
 	end
-end)
+end, nil, "Instantly upgrades upgradable machines you are looking at.")
 
 concommand.Add("jmod_ez_inv",function(ply,cmd,args)
 	if not((IsValid(ply))and(ply:Alive()))then return end
 	JMod.EZ_Open_Inventory(ply)
-end)
+end, nil, "Opens your EZ inventory to manage your armour.")
 
 concommand.Add("jmod_ez_bombdrop",function(ply,cmd,args)
 	JMod.EZ_BombDrop(ply)
-end)
+end, nil, "Drops any bombs you have armed and welded.")
 
 concommand.Add("jmod_ez_launch",function(ply,cmd,args)
 	JMod.EZ_WeaponLaunch(ply)
-end)
+end, nil, "Fires any active missiles you own.")
