3afe9fe771634e174ec41ad6d844fb8546512475
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2022-12-28 11:08:42 -0800

    Git is weird sometimes

--- lua/jmod/sh_weapons.lua
+++ lua/jmod/sh_weapons.lua
@@ -757,7 +757,6 @@ elseif SERVER then
 						if PrimSize == -1 then
 							PrimSize = -PrimSize
 						end
-						print(PrimMax)
 
 						local CurrentAmmo, ResourceLeftInBox = ply:GetAmmoCount(PrimName), ent:GetResource()
 						local SpaceLeftInPlayerInv = PrimMax - CurrentAmmo
