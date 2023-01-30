0812edb1aa23553eed156cb41c8be82a594d9195
Jackarunda <jackarunda@protonmail.com>
2023-01-29 21:49:45 -0700

    tweak

--- lua/entities/ent_jack_gmod_eznukerocket.lua
+++ lua/entities/ent_jack_gmod_eznukerocket.lua
@@ -5,7 +5,7 @@ ENT.Author = "Jackarunda"
 ENT.Category = "JMod - EZ Explosives"
 ENT.Information = "glhfggwpezpznore"
 ENT.PrintName = "EZ Nuclear Rocket"
-ENT.Spawnable = true
+ENT.Spawnable = false -- temporary, until we fix the textures and drawfunc
 ENT.AdminOnly = true
 ---
 ENT.JModPreferredCarryAngles = Angle(0, 90, 0)
