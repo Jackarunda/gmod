5e191a0605e68d197b509483917c054099104117
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2023-08-26 22:35:21 -0700

    RTG is now fuelless and soundless, except buttons of course, doubled auto shutoff time

--- lua/entities/ent_jack_gmod_ezrtg.lua
+++ lua/entities/ent_jack_gmod_ezrtg.lua
@@ -38,9 +38,7 @@ if(SERVER)then
 		self:SetProgress(0)
 		self.NextResourceThink = 0
 		self.NextUseTime = 0
-		self.NextEffThink = 0
-		self.NextEnvThink = 0
-		self.SoundLoop = CreateSound(self, "snds_jack_gmod/ezbhg_hum.wav")
+		self.TimerName = ""
 	end
 
 	function ENT:Use(activator)
@@ -58,6 +56,7 @@ if(SERVER)then
 		elseif State == STATE_ON then
 			if alt then
 				self:ProduceResource()
+				timer.Start(self.TimerName)
 				return
 			end
 			self:TurnOff()
@@ -66,24 +65,23 @@ if(SERVER)then
 
 	function ENT:TurnOn(activator)
 		if (self:GetState() ~= STATE_OFF) then JMod.Hint(activator, "destroyed", self) return end
+		self:EmitSound("buttons/button1.wav", 60, 80)
 		self.NextUseTime = CurTime() + 1
 		self:SetState(STATE_ON)
-		self.SoundLoop:SetSoundLevel(80)
-		self.SoundLoop:Play()
-		end
+		self.TimerName = ("RTGautoShutOff" .. tostring(self:EntIndex()))
+		timer.Create(self.TimerName, 1200, 1, function() 
+			if IsValid(self) then self:TurnOff() end 
+		end)
+		timer.Start(self.TimerName)
 	end
 
 	function ENT:TurnOff()
 		if (self:GetState() <= 0) then return end
 		self.NextUseTime = CurTime() + 1
-		if self.SoundLoop then self.SoundLoop:Stop() end
-		self:EmitSound("snds_jack_gmod/genny_stop.wav", 70, 100)
+		self:EmitSound("buttons/button18.wav", 60, 80)
 		self:ProduceResource()
 		self:SetState(STATE_OFF)
-	end
-
-	function ENT:OnRemove()
-		if self.SoundLoop then self.SoundLoop:Stop() end
+		timer.Remove("RTGautoShutOff" .. self.TimerName)
 	end
 
 	function ENT:ProduceResource()
@@ -92,15 +90,10 @@ if(SERVER)then
 
 		if amt <= 0 then return end
 
-		local pos = self:WorldToLocal(SelfPos + Up * 30 + Forward * 60)
-		JMod.MachineSpawnResource(self, JMod.EZ_RESOURCE_TYPES.POWER, amt, pos, Angle(0, 0, 0), Forward * 60, true, 200)
+		local pos = self:WorldToLocal(SelfPos + Up * 30 + Right * 60)
+		JMod.MachineSpawnResource(self, JMod.EZ_RESOURCE_TYPES.POWER, amt, pos, Angle(0, -90, 0), Forward * 60, true, 200)
 		self:SetProgress(math.Clamp(self:GetProgress() - amt, 0, 100))
-	end
-
-	function ENT:OnBreak()
-		if self.SoundLoop then
-			self.SoundLoop:Stop()
-		end
+		self:EmitSound("items/suitchargeok1.wav", 80, 120)
 	end
 
 	function ENT:Think()
@@ -112,24 +105,13 @@ if(SERVER)then
 			self.NextResourceThink = Time + 1
 			if State == STATE_ON then
 				local PowerPerMin = 10
-				local PowerToProduce = (60/PowerPerMin) * JMod.EZ_GRADE_BUFFS[Grade]
+				local PowerToProduce = (PowerPerMin/60) * JMod.EZ_GRADE_BUFFS[Grade]
 
 				self:SetProgress(self:GetProgress() + PowerToProduce)
 
 				if self:GetProgress() >= 100 then self:ProduceResource() end
 			end
 		end
-
-		--[[if (self.NextEffThink < Time) then
-			self.NextEffThink = Time + .1
-			if (State == STATE_ON) then
-				local Eff = EffectData()
-				Eff:SetOrigin(self:GetPos() + self:GetUp() * 65 + self:GetRight() * 11 + self:GetForward() * 35)
-				Eff:SetNormal(self:GetUp())
-				Eff:SetScale(1)
-				util.Effect("eff_jack_gmod_ezexhaust", Eff, true)
-			end
-		end--]]
 	end
 
 	function ENT:PostEntityPaste(ply, ent, createdEntities)
@@ -138,12 +120,12 @@ if(SERVER)then
 		ent.NextRefillTime = Time + math.Rand(0, 1)
 		self.NextResourceThink = Time + math.Rand(0, 3)
 		self.NextUseTime = Time + math.Rand(0, 3)
-		self.NextEffThink = Time + math.Rand(0, 3)
 	end
 
 elseif(CLIENT)then
 	function ENT:CustomInit()
 		self:DrawShadow(true)
+		self.Cylinder = JMod.MakeModel(self, "models/hunter/tubes/tube2x2x05.mdl")
 	end
 
 	local GlowSprite = Material("sprites/mat_jack_basicglow")
@@ -164,6 +146,9 @@ elseif(CLIENT)then
 		---
 		self:DrawModel()
 		---
+		local CylinderAng = SelfAng:GetCopy()
+		CylinderAng:RotateAroundAxis(Right, 0)
+		JMod.RenderModel(self.Cylinder, BasePos + Up * 18.5, CylinderAng, Vector(0.25, 0.25, 1), nil, JMod.EZ_GRADE_MATS[Grade])
 		
 		if DetailDraw then
 			if (Closeness < 20000) and (State == STATE_ON) then
@@ -174,11 +159,10 @@ elseif(CLIENT)then
 				local Opacity = math.random(50, 150)
 				local ProgFrac = self:GetProgress() / 100
 				local R, G, B = JMod.GoodBadColor(ProgFrac)
-				local FR, FG, FB = JMod.GoodBadColor(FuelFrac)
 
 				cam.Start3D2D(SelfPos + Forward * 5 + Right * 35 + Up * 20, DisplayAng, .06)
 				surface.SetDrawColor(10, 10, 10, Opacity + 50)
-				local RankX, RankY = -220, -30
+				local RankX, RankY = -65, 70
 				surface.DrawRect(RankX, RankY, 128, 128)
 				JMod.StandardRankDisplay(Grade, RankX + 62, RankY + 68, 118, Opacity + 50)
 				draw.SimpleTextOutlined("PROGRESS", "JMod-Display", 0, 0, Color(255, 255, 255, Opacity), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 3, Color(0, 0, 0, Opacity))
