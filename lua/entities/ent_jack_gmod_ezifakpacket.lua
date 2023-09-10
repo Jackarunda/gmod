5ac756a69b87034d07fef081fdcd42648d569d94
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2023-09-09 21:51:42 -0700

    ifak packet mats and model

---
+++ lua/entities/ent_jack_gmod_ezifakpacket.lua
@@ -0,0 +1,76 @@
+﻿-- Jackarunda 2023
+AddCSLuaFile()
+ENT.Type = "anim"
+ENT.Author = "Jackarunda"
+ENT.Category = "JMod - EZ Misc."
+ENT.PrintName = "EZ IFAK"
+ENT.NoSitAllowed = true
+ENT.Spawnable = true
+ENT.JModEZstorable = true
+---
+if SERVER then
+	function ENT:SpawnFunction(ply, tr)
+		local SpawnPos = tr.HitPos + tr.HitNormal * 20
+		local ent = ents.Create(self.ClassName)
+		ent:SetAngles(Angle(0, 0, 0))
+		ent:SetPos(SpawnPos)
+		ent:Spawn()
+		ent:Activate()
+
+		return ent
+	end
+
+	function ENT:Initialize()
+		self:SetModel("models/jmod/props/ifak_packet.mdl")
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetMoveType(MOVETYPE_VPHYSICS)
+		self:SetSolid(SOLID_VPHYSICS)
+		self:DrawShadow(true)
+		self:SetUseType(SIMPLE_USE)
+
+		local Phys = self:GetPhysicsObject()
+		timer.Simple(.01, function()
+			if IsValid(Phys) then
+				Phys:SetMass(8)
+				Phys:Wake()
+			end
+		end)
+	end
+
+	function ENT:OnTakeDamage(dmginfo)
+		self:TakePhysicsDamage(dmginfo)
+		local Pos, State = self:GetPos(), self:GetState()
+
+		if JMod.LinCh(dmginfo:GetDamage(), 30, 100) then
+			sound.Play("Wood_Solid.Break", Pos)
+			SafeRemoveEntityDelayed(self, 1)
+		end
+	end
+
+	function ENT:Use(activator)
+		self.LastTouchedTime = CurTime()
+		local Alt = activator:KeyDown(JMod.Config.General.AltFunctionKey)
+
+		if Alt then
+			-- Use heal
+		else
+			-- Pickup
+		end
+	end
+
+	function ENT:Think()
+		-- Haha
+	end
+
+	function ENT:OnRemove()
+		--
+	end
+	
+elseif CLIENT then
+
+	function ENT:Draw()
+		self:DrawModel()
+	end
+
+	language.Add("ent_jack_gmod_ezifakpacket", "EZ IFAK Packet")
+end
