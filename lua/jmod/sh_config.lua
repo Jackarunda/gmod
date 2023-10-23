15cca24f0991ae2df27c04d76db88fed1f978ffb
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2023-10-22 18:23:46 -0700

    Haggleing

--- lua/jmod/sh_config.lua
+++ lua/jmod/sh_config.lua
@@ -13,7 +13,7 @@ function JMod.InitGlobalConfig(forceNew, configToApply)
 		Note = "radio packages must have all lower-case names, see http://wiki.garrysmod.com/page/Enums/IN for key numbers",
 		Info = {
 			Author = "Jackarunda & Friends",
-			Version = 44.2
+			Version = 44.3
 		},
 		General = {
 			Hints = true,
@@ -1179,7 +1179,7 @@ function JMod.InitGlobalConfig(forceNew, configToApply)
 					[JMod.EZ_RESOURCE_TYPES.BASICPARTS] = 100,
 					[JMod.EZ_RESOURCE_TYPES.STEEL] = 100,
 					[JMod.EZ_RESOURCE_TYPES.PRECISIONPARTS] = 25,
-					[JMod.EZ_RESOURCE_TYPES.TUNGSTEN] = 50
+					[JMod.EZ_RESOURCE_TYPES.TUNGSTEN] = 25
 				},
 				sizeScale = 10,
 				category = "Machines",
