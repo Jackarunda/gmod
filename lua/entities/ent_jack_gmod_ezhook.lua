10116a2676fba657e30cdcc3d01e61fede715c9b
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2024-02-22 23:39:34 -0800

    Winch and hook

---
+++ lua/entities/ent_jack_gmod_ezhook.lua
@@ -0,0 +1,191 @@
+﻿-- AdventureBoots 2024
+AddCSLuaFile()
+ENT.Type = "anim"
+ENT.Author = "Jackarunda"
+ENT.Category = "JMod - EZ Misc"
+ENT.Information = "glhfggwpezpznore"
+ENT.PrintName = "EZ Hook"
+ENT.NoSitAllowed = true
+ENT.Spawnable = false
+ENT.AdminSpawnable = true
+--- func_breakable
+ENT.JModPreferredCarryAngles = Angle(0, -90, 90)
+ENT.Model = "models/jmod/ezhook01.mdl"
+ENT.EZhookType = "Hook"
+
+local STATE_BROKEN, STATE_UNHOOKED, STATE_HOOKED = -1, 0, 1
+
+function ENT:SetupDataTables()
+	self:NetworkVar("Int", 0, "State")
+end
+
+---
+if SERVER then
+	function ENT:SpawnFunction(ply, tr)
+		local SpawnPos = tr.HitPos + tr.HitNormal * 40
+		local ent = ents.Create(self.ClassName)
+		ent:SetAngles(Angle(0, 0, 0))
+		ent:SetPos(SpawnPos)
+		JMod.SetEZowner(ent, ply)
+		ent:Spawn()
+		ent:Activate()
+
+		return ent
+	end
+
+	function ENT:Initialize()
+		self:SetModel(self.Model)
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetMoveType(MOVETYPE_VPHYSICS)
+		self:SetSolid(SOLID_VPHYSICS)
+		self:DrawShadow(true)
+		self:SetUseType(ONOFF_USE)
+
+		if self:GetPhysicsObject():IsValid() then
+			self:GetPhysicsObject():SetMass(15)
+			self:GetPhysicsObject():Wake()
+		end
+
+		---
+		self:SetState(STATE_UNHOOKED)
+		self.NextStick = 0
+	end
+
+	function ENT:PhysicsCollide(data, physobj)
+		local Time = CurTime()
+		if data.DeltaTime > 0.2 and data.Speed > 25 then
+			self:EmitSound("snd_jack_claythunk.wav", 55, math.random(80, 120))
+			if self:IsPlayerHolding() then
+				timer.Simple(0, function()
+					if self.EZhookType == "Plugin" then
+						local Ent = data.HitEntity
+						local Connected = JMod.CreateConnection(self.EZconnector, Ent, self.EZconnector.MaxConnectionRange or 1000)
+						if Connected then SafeRemoveEntity(self) end
+					else
+						self.NextStick = Time + .5
+						local Ang = data.HitNormal:Angle()
+						Ang:RotateAroundAxis(Ang:Right(), 90)
+						self:SetAngles(Ang)
+						self:SetPos(data.HitPos)
+		
+						-- crash prevention
+						if data.HitEntity:GetClass() == "func_breakable" then
+							timer.Simple(0, function()
+								self:GetPhysicsObject():Sleep()
+							end)
+						else
+							local Weld = constraint.Weld(self, data.HitEntity, 0, 0, 5000, true, false)
+							self.StuckTo = data.HitEntity
+							self.StuckStick = Weld
+						end
+		
+						self:EmitSound("snd_jack_claythunk.wav", 65, math.random(80, 120))
+						self:SetState(STATE_HOOKED)
+						self:SetBodygroup(0, 0)
+						DropEntityIfHeld(self)
+						JMod.Hint(Dude, "arm")
+					end
+				end)
+			end
+		end
+	end
+
+	function ENT:OnTakeDamage(dmginfo)
+		if dmginfo:GetInflictor() == self then return end
+		self:TakePhysicsDamage(dmginfo)
+		local Dmg = dmginfo:GetDamage()
+
+		if not(self:GetState() == STATE_BROKEN) and JMod.LinCh(Dmg, 30, 100) then
+			sound.Play("Metal_Box.Break", Pos)
+			self:SetState(STATE_BROKEN)
+			SafeRemoveEntityDelayed(self, 10)
+		end
+	end
+
+	function ENT:OnHook(Dude)
+		local Time = CurTime()
+		local Tr = util.QuickTrace(Dude:GetShootPos(), Dude:GetAimVector() * 80, {self, Dude})
+
+		if Tr.Hit and IsValid(Tr.Entity:GetPhysicsObject()) and not(Tr.Entity:IsNPC()) and not(Tr.Entity:IsPlayer()) then
+			if self.EZhookType == "Plugin" then
+				local Ent = Tr.Entity
+				local Connected = JMod.CreateConnection(self.EZconnector, Ent, self.EZconnector.MaxConnectionRange or 1000)
+				if Connected then SafeRemoveEntity(self) end
+			else
+				self.NextStick = Time + .5
+				local Ang = Tr.HitNormal:Angle()
+				Ang:RotateAroundAxis(Ang:Right(), -90)
+				Ang:RotateAroundAxis(Ang:Up(), 90)
+				self:SetAngles(Ang)
+				self:SetPos(Tr.HitPos)
+
+				-- crash prevention
+				if Tr.Entity:GetClass() == "func_breakable" then
+					timer.Simple(0, function()
+						self:GetPhysicsObject():Sleep()
+					end)
+				else
+					local Weld = constraint.Weld(self, Tr.Entity, 0, Tr.PhysicsBone, 5000, true, false)
+					self.StuckTo = Tr.Entity
+					self.StuckStick = Weld
+				end
+
+				self:EmitSound("snd_jack_claythunk.wav", 65, math.random(80, 120))
+				self:SetState(STATE_HOOKED)
+				self:SetBodygroup(0, 0)
+				Dude:DropObject()
+				JMod.Hint(Dude, "arm")
+			end
+		end
+	end
+
+	function ENT:Use(activator, activatorAgain, onOff)
+		local Dude = activator or activatorAgain
+		if not IsValid(Dude) then return end
+		JMod.SetEZowner(self, Dude)
+		local Time = CurTime()
+
+		if tobool(onOff) then
+			local State = self:GetState()
+			if State < 0 then return end
+			local Alt = Dude:KeyDown(JMod.Config.General.AltFunctionKey)
+			self:SetBodygroup(0, 1)
+
+			if State == STATE_UNHOOKED then
+				if self.StuckStick then SafeRemoveEntity(self.StuckStick) end
+				self.StuckStick = nil
+				self.StuckTo = nil
+				Dude:PickupObject(self)
+				self.NextStick = Time + .5
+				JMod.Hint(Dude, "sticky")
+			else
+				self:EmitSound("snd_jack_minearm.wav", 60, 70)
+				self:SetState(STATE_UNHOOKED)
+			end
+		else
+			self:SetBodygroup(0, 0)
+		end
+	end
+
+	function ENT:Think()
+		if istable(WireLib) then
+			WireLib.TriggerOutput(self, "State", self:GetState())
+		end
+	end
+
+	function ENT:OnRemove()
+	end
+
+elseif CLIENT then
+	function ENT:Initialize()
+	end
+
+	--
+	local GlowSprite = Material("sprites/mat_jack_basicglow")
+
+	function ENT:Draw()
+		self:DrawModel()
+	end
+
+	language.Add("ent_jack_gmod_ezhook", "EZ Hook")
+end
---
+++ lua/entities/ent_jack_gmod_ezwinch.lua
@@ -0,0 +1,136 @@
+-- AdventureBoots 2024
+AddCSLuaFile()
+ENT.Type = "anim"
+ENT.Base = "ent_jack_gmod_ezmachine_base"
+ENT.PrintName = "EZ Winch"
+ENT.Author = "Jackarunda"
+ENT.Category = "JMod - EZ Machines"
+ENT.Information = ""
+ENT.Spawnable = false
+ENT.AdminSpawnable = true
+--
+ENT.JModPreferredCarryAngles = Angle(0, 0, 0)
+ENT.Model = "models/props_lab/citizenradio.mdl"
+ENT.Mass = 50
+--
+ENT.StaticPerfSpecs={ 
+	MaxElectricity = 100,
+	MaxDurability = 100,
+	Armor = 1.5
+}
+
+if SERVER then
+	function ENT:SpawnFunction(ply, tr)
+		local SpawnPos = tr.HitPos + tr.HitNormal * 20
+		local ent = ents.Create(self.ClassName)
+		ent:SetPos(SpawnPos)
+		JMod.SetEZowner(ent, ply)
+		ent:Spawn()
+		ent:Activate()
+		JMod.Hint(JMod.GetEZowner(ent), "ent_jack_gmod_ezwinch")
+		return ent
+	end
+
+	function ENT:CustomInit()
+		self.NextUseTime = 0
+		self.EZconnections = {}
+		self.EZupgradable = false
+		self.EZcolorable = false
+	end
+
+	function ENT:Use(activator)
+		if self.NextUseTime > CurTime() then return end
+		local State = self:GetState()
+		local IsPly = (IsValid(activator) and activator:IsPlayer())
+		local Alt = IsPly and activator:KeyDown(JMod.Config.General.AltFunctionKey)
+		JMod.SetEZowner(self, activator)
+
+		if State == JMod.EZ_STATE_BROKEN then
+			JMod.Hint(activator, "destroyed", self)
+		end
+		
+		if Alt then
+		else
+			if IsValid(self.EZhooky) then 
+				if self.EZhooky:IsPlayerHolding() then return end
+				SafeRemoveEntity(self.EZhooky)
+			end
+
+			local Hooky = ents.Create("ent_jack_gmod_ezhook")
+			if not IsValid(Hooky) then return end
+			Hooky:SetPos(self:GetPos() + Vector(0, 0, 50)) -- Adjust the position as needed
+			Hooky:SetAngles(self:GetAngles())
+			Hooky.EZconnector = machine
+			Hooky:Spawn()
+			Hooky:Activate()
+			Hooky.NextStick = CurTime() + 1
+			self.EZhooky = Hooky
+
+			local ropeLength = self.MaxConnectionRange or 1000
+			constraint.Rope(self, Hooky, 0, 0, Vector(0,0,0), Vector(0,0,9), ropeLength, 0, 1000, 2, "cable/cable2", false)
+			
+			activator:DropObject()
+			activator:PickupObject(Hooky)
+		end
+	end
+
+	function ENT:TurnOn(dude)
+		if self:GetState() ~= JMod.EZ_STATE_OFF then return end
+		self:SetState(JMod.EZ_STATE_ON)
+		if not(IsValid(dude) and dude:IsPlayer()) then return end
+	end
+
+	function ENT:TurnOff()
+		if self:GetState() ~= JMod.EZ_STATE_ON then return end
+		self:SetState(JMod.EZ_STATE_OFF)
+	end
+
+	function ENT:Think()
+		local Time, State = CurTime(), self:GetState()
+
+		if (State == JMod.EZ_STATE_ON) then
+			
+		end
+
+		self:NextThink(Time + 1)
+		return true
+	end
+
+elseif CLIENT then
+	function ENT:CustomInit()
+		self:DrawShadow(true)
+	end
+
+	function ENT:Draw()
+		local SelfPos, SelfAng, State, FT = self:GetPos(), self:GetAngles(), self:GetState(), FrameTime()
+		local Up, Right, Forward = SelfAng:Up(), SelfAng:Right(), SelfAng:Forward()
+		---
+		local BasePos = SelfPos
+		local Obscured = util.TraceLine({start = EyePos(), endpos = BasePos, filter = {LocalPlayer(), self}, mask = MASK_OPAQUE}).Hit
+		local Closeness = LocalPlayer():GetFOV() * (EyePos():Distance(SelfPos))
+		local DetailDraw = Closeness < 120000 -- cutoff point is 400 units when the fov is 90 degrees
+		---
+		if((not(DetailDraw)) and (Obscured))then return end -- if player is far and sentry is obscured, draw nothing
+		if(Obscured)then DetailDraw = false end -- if obscured, at least disable details
+		if(State == STATE_BROKEN)then DetailDraw = false end -- look incomplete to indicate damage, save on gpu comp too
+		---
+		self:DrawModel()
+		---
+		if DetailDraw then
+			if Closeness < 20000 and State == JMod.EZ_STATE_ON then
+				local DisplayAng = SelfAng:GetCopy()
+				DisplayAng:RotateAroundAxis(DisplayAng:Right(), -90)
+				DisplayAng:RotateAroundAxis(DisplayAng:Up(), 90)
+				local Opacity = math.random(50, 150)
+				local Elec = self:GetElectricity()
+				local R, G, B = JMod.GoodBadColor(Elec / 1000)
+
+				cam.Start3D2D(SelfPos + Forward * 10.5 + Up * 23, DisplayAng, .08)
+				draw.SimpleTextOutlined("POWER", "JMod-Display", 0, 0, Color(200, 255, 255, Opacity), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 3, Color(0, 0, 0, Opacity))
+				draw.SimpleTextOutlined(tostring(math.Round(Elec)) .. "/" .. tostring(math.Round(self.MaxElectricity)), "JMod-Display", 0, 30, Color(R, G, B, Opacity), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 3, Color(0, 0, 0, Opacity))
+				cam.End3D2D()
+			end
+		end
+		language.Add("ent_jack_gmod_ezwinch", "EZ Winch")
+	end
+end
