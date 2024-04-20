7b6aa9d2c46124136939d38a0c96b2eaeaa8d2b6
Jackarunda <jackarunda@protonmail.com>
2024-04-19 21:21:07 -0700

    adding nice liquid stream effect

---
+++ lua/effects/eff_jack_gmod_liquidtrail.lua
@@ -0,0 +1,79 @@
+local StreamSprite = Material("effects/bloodstream")
+local GlowSprite = Material("sprites/mat_jack_basicglow")
+
+function EFFECT:Init(data)
+	local Dir = data:GetNormal()
+	local Pos = data:GetOrigin()
+	local Time = CurTime()
+	self.DieTime = Time + 2
+	self.Particles = {}
+	self.NumParticles = 10
+	for i = 1, self.NumParticles do
+		local Fraction = i / self.NumParticles
+		local InverseFraction = 1 - Fraction
+		table.insert(self.Particles, {
+			size = 1,
+			opacity = 255,
+			pos = Pos,
+			vel = Dir * Fraction * 1500 + VectorRand() * 15,
+			airResist = InverseFraction * 3,
+			stuck = false,
+			posParticle = i == math.ceil(self.NumParticles * .6),
+			growthSpeed = i
+		})
+	end
+end
+
+function EFFECT:Think()
+	local Time, FT = CurTime(), FrameTime()
+	if (self.DieTime < Time) then return false end
+	for k, v in pairs(self.Particles) do
+		if not (v.stuck) then
+			local Travel = v.vel * FT
+			local Tr = util.TraceLine({
+				start = v.pos,
+				endpos = v.pos + Travel,
+				mask = MASK_NPCWORLDSTATIC
+			})
+			if (Tr.Hit) then
+				v.pos = Tr.HitPos + Tr.HitNormal
+				v.stuck = true
+			else
+				v.pos = v.pos + Travel
+			end
+			if (v.posParticle) then self.Entity:SetPos(v.pos) end
+			v.vel = v.vel - Vector(0, 0, 600  * FT)
+			local AirLoss = FT * v.airResist + .01
+			v.vel = v.vel * (1 - AirLoss)
+			v.vel = v.vel + JMod.Wind * FT * 100
+		end
+		v.size = v.size + v.growthSpeed * .05 * ((v.stuck and 2) or 1)
+	end
+	return true
+end
+
+function EFFECT:Render()
+	render.SetMaterial(StreamSprite)
+	local LastPos, Count = nil, 0
+	for k, v in pairs(self.Particles) do
+		if (v.size < 80) then
+			local Sine = math.sin((Count / (self.NumParticles - 2)) * math.pi)
+			if (LastPos) then
+				local R = math.Clamp(175 + v.size * 1.5, 0, 255)
+				local G = math.Clamp(200 + v.size * 1.5, 0, 255)
+				local Col = Color(R, G, 255, 255 - math.Clamp(v.size * 4, 0, 255))
+				render.DrawBeam(LastPos, v.pos, v.size ^ 1.1, 1, 0, Col)
+			end
+			LastPos = v.pos
+			Count = Count + 1
+		end
+	end
+
+	--[[
+	render.SetMaterial(GlowSprite)
+	for k, v in pairs(self.Particles) do
+		render.DrawSprite(v.pos, 32, 32, color_white)
+	end
+	--]]
+	-- render.DrawSprite(self.Entity:GetPos(), 32, 32, color_white)
+end
