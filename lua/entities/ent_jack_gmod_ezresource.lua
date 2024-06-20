80700bc76f88092297f53e22b1b8d2eccc53deb7
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2024-06-19 21:54:17 -0700

    Heh, need to move code because sorse

--- lua/entities/ent_jack_gmod_ezresource.lua
+++ lua/entities/ent_jack_gmod_ezresource.lua
@@ -110,11 +110,11 @@ if SERVER then
 				if self.PhysMat then
 					Phys:SetMaterial(self.PhysMat)
 				end
+				Phys:SetMass(math.max(self.Mass))
+				Phys:Wake()
 				if self.EZbuoyancy then
 					Phys:SetBuoyancyRatio(self.EZbuoyancy)
 				end
-				Phys:SetMass(math.max(self.Mass))
-				Phys:Wake()
 			end
 		end)
 
