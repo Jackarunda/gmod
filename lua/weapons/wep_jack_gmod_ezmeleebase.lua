42a930f5650e0abf607322d2dd57377950493321
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2024-06-24 21:20:05 -0700

    Fixed the melee sweps

--- lua/weapons/wep_jack_gmod_ezmeleebase.lua
+++ lua/weapons/wep_jack_gmod_ezmeleebase.lua
@@ -7,7 +7,7 @@ SWEP.Purpose = ""
 SWEP.Spawnable = false
 SWEP.UseHands = true
 SWEP.DrawAmmo = false
-SWEP.DrawCrosshair = true
+SWEP.DrawCrosshair = false
 SWEP.EZdroppable = true
 SWEP.ViewModel = ""
 SWEP.WorldModel = ""
@@ -35,6 +35,8 @@ SWEP.DropEnt = ""
 --
 SWEP.HitDistance		= 50
 SWEP.HitInclination		= 0.4
+SWEP.HitHeight 			= 0
+SWEP.HitAngle 			= 45
 SWEP.HitPushback		= 2000
 SWEP.MaxSwingAngle		= 120
 SWEP.SwingSpeed 		= 1
@@ -53,6 +55,7 @@ SWEP.PushSoundBody 	= Sound( "Flesh.ImpactSoft" )
 --
 SWEP.IdleHoldType 	= "melee2"
 SWEP.SprintHoldType = "melee2"
+SWEP.HideViewModel = false
 --
 
 function SWEP:Initialize()
@@ -77,7 +80,7 @@ function SWEP:PrimaryAttack()
 	if self.SprintCancel and Owner:KeyDown(IN_SPEED) then return end
 	if self:GetSwinging() then return end
 
-	self:SetNextPrimaryFire(CurTime() + 1.1)
+	self:SetNextPrimaryFire(CurTime() + self.PrimaryAttackSpeed)
 	self:SetNextSecondaryFire(CurTime() + 1)
 
 	local IsPlaya = Owner:IsPlayer()
@@ -107,7 +110,7 @@ function SWEP:Pawnch()
 	Owner:SetAnimation(PLAYER_ATTACK1)
 	local vm = Owner:GetViewModel()
 	vm:SendViewModelMatchingSequence(vm:LookupSequence("misscenter1"))
-	Owner:ViewPunch(Angle(math.random(-10, -5), math.random(-5, 0), 0))
+	--Owner:ViewPunch(Angle(math.random(-10, -5), math.random(-5, 0), 0))
 	self:UpdateNextIdle()
 end
 
@@ -178,7 +181,7 @@ function SWEP:Think()
 
 					local SwingPos = Owner:GetShootPos()
 					local SwingAng = Owner:EyeAngles()
-					SwingAng:RotateAroundAxis(SwingAng:Forward(), 45)
+					SwingAng:RotateAroundAxis(SwingAng:Forward(), self.HitAngle)
 					SwingAng:RotateAroundAxis(SwingAng:Right(), math.deg(SwingCos))
 					SwingAng:RotateAroundAxis(SwingAng:Up(), 8)
 
@@ -188,7 +191,7 @@ function SWEP:Think()
 
 					local tr = util.TraceLine( {
 						start = (SwingPos + Offset),
-						endpos = (SwingPos + Offset) + SwingForward * self.HitDistance + SwingRight * -self.HitInclination,
+						endpos = (SwingPos + Offset) + SwingForward * self.HitDistance + SwingRight * -self.HitInclination + SwingUp * self.HitHeight,
 						filter = Owner,
 						mask = MASK_SHOT_HULL
 					})
@@ -346,7 +349,9 @@ function SWEP:Deploy()
 end
 
 function SWEP:PreDrawViewModel(vm, wep, ply)
-	--vm:SetMaterial("engine/occlusionproxy") -- Hide that view model with hacky material
+	if self.HideViewModel then
+		vm:SetMaterial("engine/occlusionproxy") -- Hide that view model with hacky material
+	end
 end
 
 function SWEP:ViewModelDrawn()
@@ -364,8 +369,10 @@ function SWEP:GetViewModelPosition(pos, ang)
 
 	if (self.Owner:KeyDown(IN_SPEED)) or (self.Owner:KeyDown(IN_ZOOM)) then
 		Downness = Lerp(FT * 2, Downness, 5)
+	elseif (self:GetSwinging()) then
+		Downness = Lerp(FT * 2, Downness, -1)
 	else
-		Downness = Lerp(FT * 2, Downness, -2)
+		Downness = Lerp(FT * 2, Downness, -3)
 	end
 
 	ang:RotateAroundAxis(ang:Right(), -Downness * 5)
