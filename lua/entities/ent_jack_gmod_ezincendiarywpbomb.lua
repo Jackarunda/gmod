fef71612422efa10e3c507a85a0329a4438ed368
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2024-07-12 17:15:57 -0700

    W I P White Incendiary Phosphorus

---
+++ lua/entities/ent_jack_gmod_ezincendiarywpbomb.lua
@@ -0,0 +1,123 @@
+﻿-- Jackarunda 2021
+AddCSLuaFile()
+ENT.Base = "ent_jack_gmod_ezbomb"
+ENT.Author = "Jackarunda"
+ENT.Category = "JMod - EZ Explosives"
+ENT.Information = "glhfggwpezpznore"
+ENT.PrintName = "EZ White Phosphorus Bomb"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+---
+ENT.JModPreferredCarryAngles = Angle(0, 0, 0)
+ENT.EZRackOffset = Vector(0, 0, 10)
+ENT.EZRackAngles = Angle(0, 0, 0)
+ENT.EZbombBaySize = 5
+---
+ENT.EZguidable = false
+ENT.Model = "models/props_phx/ww2bomb.mdl"
+ENT.Material = "models/entities/mat_jack_wpinbomb"
+ENT.Mass = 100
+ENT.DetSpeed = 1000
+ENT.DetType = "airburst"
+
+local STATE_BROKEN, STATE_OFF, STATE_ARMED = -1, 0, 1
+
+---
+if SERVER then
+	function ENT:Detonate()
+		if self.Exploded then return end
+		self.Exploded = true
+		local SelfPos, Att = self:GetPos() + Vector(0, 0, 30), JMod.GetEZowner(self)
+		JMod.Sploom(Att, SelfPos, 100)
+		---
+		util.ScreenShake(SelfPos, 1000, 3, 2, 1000)
+		---
+		local Dir = self:GetPhysicsObject():GetVelocity():GetNormalized()
+		local Speed = math.Clamp(self:GetPhysicsObject():GetVelocity():Length(), 0, self.DetSpeed * .5)
+		---
+		local Sploom = EffectData()
+		Sploom:SetOrigin(SelfPos)
+		Sploom:SetScale(.6)
+		Sploom:SetNormal(Dir)
+		util.Effect("eff_jack_firebomb", Sploom, true, true)
+
+		---
+		local Owner = JMod.GetEZowner(self)
+		for i = 1, 25 do
+			timer.Simple(i / 50, function()
+				local FireAng = (Dir + VectorRand() * .35 + Vector(0, 0, math.Rand(.01, .7))):Angle()
+				local Flame = ents.Create("ent_jack_gmod_eznapalm")
+				Flame.Creator = self
+				Flame:SetPos(SelfPos)
+				Flame:SetAngles(FireAng)
+				Flame:SetOwner(self)
+				JMod.SetEZowner(Flame, Owner)
+				Flame.InitialVel = Dir * Speed
+				Flame.HighVisuals = math.random(1, 5) == 1
+				Flame:Spawn()
+				Flame:Activate()
+			end)
+		end
+		for i = 1, 20 do
+			timer.Simple(i / 10, function()
+				local FireDir = (Dir + VectorRand() * .35 + Vector(0, 0, math.Rand(.01, .7)))
+				local Gas = ents.Create("ent_jack_gmod_ezwpincendiaryparticle")
+				Gas.Creator = self
+				Gas:SetPos(SelfPos)
+				Gas:SetOwner(self)
+				JMod.SetEZowner(Gas, Owner)
+				Gas.CurVel = FireDir * Speed
+				Gas.Canister = self
+				Gas:Spawn()
+				Gas:Activate()
+				Gas.AffectRange = 500
+				Gas.MaxLife = math.random(50, Gas.MaxLife)
+			end)
+		end
+
+		---
+		timer.Simple(0, function()
+			if IsValid(self) then
+				self:Remove()
+			end
+		end)
+	end
+
+	function ENT:AeroDragThink()
+
+		local Phys = self:GetPhysicsObject()
+
+		if (self:GetState() == STATE_ARMED) and (Phys:GetVelocity():Length() > 400) and not self:IsPlayerHolding() and not constraint.HasConstraints(self) then
+			self.FreefallTicks = self.FreefallTicks + 1
+
+			if self.FreefallTicks >= 10 then
+				local Tr = util.QuickTrace(self:GetPos(), Phys:GetVelocity():GetNormalized() * 1000, self)
+
+				if Tr.Hit then
+					self:Detonate()
+				end
+			end
+		else
+			self.FreefallTicks = 0
+		end
+
+		JMod.AeroDrag(self, self:GetForward())
+		self:NextThink(CurTime() + .1)
+
+		return true
+	end
+elseif CLIENT then
+	function ENT:Initialize()
+	end
+
+	--
+	function ENT:Think()
+	end
+
+	--
+	function ENT:Draw()
+		self:DrawModel()
+	end
+
+	language.Add("ent_jack_gmod_ezincendiarybomb", "EZ Incendiary Bomb")
+end
---
+++ lua/entities/ent_jack_gmod_ezwpincendiaryparticle.lua
@@ -0,0 +1,164 @@
+﻿-- Based off of JMOD EZ Gas Particle, created by Freaking Fission, uses some code from GChem
+AddCSLuaFile()
+ENT.Base = "ent_jack_gmod_ezgasparticle"
+ENT.PrintName = "EZ WP Cloud"
+ENT.Author = "Jackarunda, AdventureBoots, Freaking Fission"
+ENT.NoSitAllowed = true
+ENT.Editable = false
+ENT.Spawnable = false
+ENT.AdminSpawnable = false
+ENT.AdminOnly = false
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+--
+ENT.ThinkRate = 2
+ENT.AffectRange = 250
+ENT.MaxLife = 100
+ENT.MaxVel = 150
+--
+
+if SERVER then
+	function ENT:CustomInit()
+		local Time = CurTime()
+		self.NextDmg = Time + 2.5
+		self.CurVel = self.CurVel or VectorRand() * 50
+		self.AirResistance = 1
+	end
+
+	function ENT:DamageObj(obj)
+		local Time = CurTime()
+		if obj:IsPlayer() then
+			local faceProt, skinProt = JMod.GetArmorBiologicalResistance(obj, DMG_NERVEGAS)
+
+			JMod.DepleteArmorChemicalCharge(obj, (faceProt + skinProt) * 4 * .02)
+
+			if faceProt < 1 then
+				net.Start("JMod_VisionBlur")
+				net.WriteFloat(5 * math.Clamp(1 - faceProt, 0, 1))
+				net.WriteFloat(2)
+				net.WriteBit(false)
+				net.Send(obj)
+				JMod.Hint(obj, "tear gas")
+				JMod.TryCough(obj)
+			end
+
+			if math.random(1, 2) == 1 and (faceProt + skinProt < 2) then
+				local Dmg = DamageInfo()
+				Dmg:SetDamageType(DMG_BURN + DMG_NERVEGAS)
+				Dmg:SetDamage(math.random(1, 4) * JMod.Config.Particles.PoisonGasDamage)
+				Dmg:SetInflictor(self)
+				Dmg:SetAttacker(JMod.GetEZowner(self))
+				Dmg:SetDamagePosition(obj:GetPos())
+				obj:TakeDamageInfo(Dmg)
+				JMod.TryCough(obj)
+			end
+		elseif obj:IsNPC() then
+			obj.EZNPCincapacitate = Time + math.Rand(2, 5)
+			local Dmg = DamageInfo()
+			Dmg:SetDamageType(DMG_BURN)
+			Dmg:SetDamage(math.random(1, 4) * JMod.Config.Particles.PoisonGasDamage)
+			Dmg:SetInflictor(self)
+			Dmg:SetAttacker(JMod.GetEZowner(self))
+			Dmg:SetDamagePosition(obj:GetPos())
+			obj:TakeDamageInfo(Dmg)
+		end
+	end
+
+	function ENT:CalcMove(ThinkRateHz)
+		local SelfPos, Time = self:GetPos(), CurTime()
+		local RandDir = VectorRand(-6, 6)
+		RandDir.z = RandDir.z * .5
+		local Force = RandDir + (JMod.Wind * 2) + Vector(0, 0, -10)
+
+		for key, obj in pairs(ents.FindInSphere(SelfPos, self.AffectRange)) do
+			if math.random(1, 2) == 1 and not (obj == self) and self:CanSee(obj) then
+				if obj.EZgasParticle and not(obj.EZvirusParticle) then
+					-- repel in accordance with Ideal Gas Law
+					local Vec = (obj:GetPos() - SelfPos):GetNormalized()
+					Force = Force - Vec * .2
+				elseif self.NextDmg < Time and self:ShouldDamage(obj) then
+					self:DamageObj(obj)
+				end
+			end
+		end
+	
+		-- apply acceleration
+		self.CurVel = self.CurVel + Force / ThinkRateHz
+
+		-- apply air resistance
+		--self.CurVel = self.CurVel / 1.5
+
+		-- apply max velocity
+		self.CurVel = self.CurVel:GetNormalized() * math.min(self.CurVel:Length(), self.MaxVel)
+
+		-- observe current velocity
+		local NewPos = SelfPos + self.CurVel / ThinkRateHz
+
+		-- make sure we're not gonna hit something. If so, bounce
+		local MoveTrace = util.TraceLine({
+			start = SelfPos,
+			endpos = NewPos,
+			filter = { self, self.Canister },
+			mask = MASK_SHOT
+		})
+		if not MoveTrace.Hit then
+			-- move unobstructed
+			self:SetPos(NewPos + MoveTrace.HitNormal * 1)
+		else
+			-- bounce in accordance with Ideal Gas Law
+			self:SetPos(MoveTrace.HitPos + MoveTrace.HitNormal * 10)
+			local CurVelAng, Speed = self.CurVel:Angle(), self.CurVel:Length() * .3
+			CurVelAng:RotateAroundAxis(MoveTrace.HitNormal, 180)
+			local H = Vector(self.CurVel.x, self.CurVel.y, self.CurVel.z)
+			self.CurVel = -(CurVelAng:Forward() * Speed)
+		end
+	end
+
+elseif CLIENT then
+	local Mat = Material("effects/smoke_b")
+	local DebugMat = Material("sprites/mat_jack_jackconfetti")
+
+	function ENT:Initialize()
+		self.Col = Color(255, 255, 255)
+		self.Visible = true
+		self.Show = true
+		self.siz = .5
+		self.RenderPos = self:GetPos()
+
+		timer.Simple(2, function()
+			if IsValid(self) then
+				self.Visible = math.random(1, 2) == 2
+			end
+		end)
+
+		self.NextVisCheck = CurTime() + 6
+		self.DebugShow = LocalPlayer().EZshowGasParticles or false
+		self.Rotation = math.random(0, 360)
+		
+		self:SetModelScale(2)
+	end
+
+	function ENT:DrawTranslucent()
+		self.DebugShow = LocalPlayer().EZshowGasParticles or false
+		if self.DebugShow then
+			render.SetMaterial(DebugMat)
+			render.DrawSprite(self:GetPos(), 50, 50, Color(255, 255, 255, 200))
+		end
+
+		local Time = CurTime()
+
+		if self.NextVisCheck < Time then
+			self.NextVisCheck = Time + 1
+			self.Show = self.Visible and 1 / FrameTime() > 50
+		end
+		self.Show = self.Visible
+
+		--if self.Show then
+			self.Rotation = self.Rotation + FrameTime() * math.Rand(1, 10)
+			local SelfPos = self:GetPos()
+			render.SetMaterial(Mat)
+			render.DrawQuadEasy(self.RenderPos, LocalPlayer():EyePos() - SelfPos, self.siz, self.siz, Color(self.Col.r, self.Col.g, self.Col.b, 150), self.Rotation)
+			self.RenderPos = LerpVector(FrameTime() * .5, self.RenderPos, SelfPos)
+			self.siz = math.Clamp(self.siz + FrameTime() * 200, 0, 500)
+		--end
+	end
+end
