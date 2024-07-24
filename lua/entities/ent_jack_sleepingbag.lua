35f706ee57de91bd3d77539adebd56fd20aa811e
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2024-07-23 21:10:28 -0700

    Rewrote sleeping bag logic (again)

--- lua/entities/ent_jack_sleepingbag.lua
+++ lua/entities/ent_jack_sleepingbag.lua
@@ -7,35 +7,45 @@ ENT.Spawnable = true
 ENT.EZrespawnPoint = true
 ENT.Mass = 35
 ENT.JModEZstorable = true
+ENT.JModPreferredCarryAngles = Angle(0, -90, 90)
 
 local STATE_ROLLED, STATE_UNROLLED = 0, 1
 local MODEL_ROLLED, MODEL_UNROLLED = "models/jmod/props/sleeping_bag_rolled.mdl","models/jmod/props/sleeping_bag.mdl"
 local ClothSounds = {"snds_jack_gmod/equip1.ogg", "snds_jack_gmod/equip2.ogg", "snds_jack_gmod/equip3.ogg", "snds_jack_gmod/equip4.ogg", "snds_jack_gmod/equip5.ogg"}
 
-if (CLIENT) then
-	function ENT:Draw()
-		self:DrawModel()
+if SERVER then
+	function ENT:SpawnFunction(ply, tr)
+		local SpawnPos = tr.HitPos + tr.HitNormal * 20
+		local ent = ents.Create(self.ClassName)
+		ent:SetPos(SpawnPos)
+		ent:SetAngles(ent.JModPreferredCarryAngles)
+		ent:Spawn()
+		ent:Activate()
+		--JMod.SetEZowner(ent, ply)
+		-- local effectdata=EffectData()
+		-- effectdata:SetEntity(ent)
+		-- util.Effect("propspawn",effectdata)
+		return ent
 	end
-elseif (SERVER) then
 
 	function ENT:Initialize()
 		self.State = STATE_ROLLED
 		self:SetModel(MODEL_ROLLED)
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetMoveType(MOVETYPE_VPHYSICS)
-		self:SetSolid( SOLID_VPHYSICS )
-		JMod.SetEZowner(self, nil)
-		local phys = self:GetPhysicsObject()
-		if phys:IsValid()then
-			phys:Wake()
-			phys:SetMass(35)
-			self:SetColor(Color(100, 100, 100))
-		end
-		
+		self:SetSolid(SOLID_VPHYSICS)
 		self:SetUseType(SIMPLE_USE)
+
+		local Phys = self:GetPhysicsObject()
+		timer.Simple(0, function()
+			if Phys:IsValid()then
+				Phys:Wake()
+				Phys:SetMass(self.Mass)
+			end
+		end)
 		
 		self.nextSpawnTime = 0
-		--self:CreatePod()
+		self:SetColor(Color(100, 100, 100))
 	end
 
 	function ENT:CreatePod()
@@ -44,12 +54,11 @@ elseif (SERVER) then
 			self.Pod:Fire("kill")
 			self.Pod = nil
 		end
+		local Ang, Up, Right, Forward = self:GetAngles(), self:GetUp(), self:GetRight(), self:GetForward()
+
 		self.Pod = ents.Create("prop_vehicle_prisoner_pod")
 		self.Pod:SetModel("models/vehicles/prisoner_pod_inner.mdl")
-		local Ang, Up, Right, Forward = self:GetAngles(), self:GetUp(), self:GetRight(), self:GetForward()
 		self.Pod:SetPos(self:GetPos()+Up*12-Right*1+Forward*45)
-		--Ang:RotateAroundAxis(Up, 0)
-		--Ang:RotateAroundAxis(Forward, 0)
 		Ang:RotateAroundAxis(Right, 85)
 		self.Pod:SetAngles(Ang)
 		self.Pod:Spawn()
@@ -58,17 +67,10 @@ elseif (SERVER) then
 		self.Pod:SetNoDraw(true)
 		self.Pod:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
 		self.Pod:Fire("lock")
-		--self.Pod.IsJackyPod = true
-		--self.Pod.EZvehicleEjectPos = 
-		--self.Pod:SetNotSolid(true)
-		--self.Pod:Fire("lock", "", 0)
-		--self.Pod:SetThirdPersonMode(false)
-		--self.Pod:SetCameraDistance(0)
 	end
 	
 	function ENT:RollUp()
 		self.State = STATE_ROLLED
-		--JMod.SetEZowner(self, nil)
 		if(IsValid(self.Pod))then
 			self.Pod:SetParent(nil)
 			self.Pod:Fire("kill")
@@ -80,16 +82,18 @@ elseif (SERVER) then
 		self:SetMoveType(MOVETYPE_VPHYSICS)    
 		self:SetSolid(SOLID_VPHYSICS)
 		self:DrawShadow(true)
-		self:SetUseType(SIMPLE_USE)
 
 		sound.Play("snd_jack_clothequip.ogg", self:GetPos(), 65, math.random(90, 110))
 		
-		local phys = self:GetPhysicsObject()
-		if phys:IsValid() then
-			phys:Wake()
-			phys:SetMass(self.Mass)
-		end
+		local Phys = self:GetPhysicsObject()
+		timer.Simple(0, function()
+			if Phys:IsValid()then
+				Phys:Wake()
+				Phys:SetMass(self.Mass)
+			end
+		end)
 		self:SetPos(self:GetPos() + Vector(0, 0, 20))
+		self:SetAngles(self:GetAngles() + Angle(0, -90, 0))
 	end
 
 	function ENT:UnRoll()
@@ -100,13 +104,15 @@ elseif (SERVER) then
 		self:SetMoveType(MOVETYPE_VPHYSICS)    
 		self:SetSolid(SOLID_VPHYSICS)
 		self:DrawShadow(true)
-		self:SetUseType(SIMPLE_USE)
 		
-		local phys = self:GetPhysicsObject()
-		if phys:IsValid() then
-			phys:Wake()
-			phys:SetMass(self.Mass)
-		end
+		local Phys = self:GetPhysicsObject()
+		timer.Simple(0, function()
+			if Phys:IsValid()then
+				Phys:Wake()
+				Phys:SetMass(self.Mass)
+			end
+		end)
+
 		local SelfPos = self:LocalToWorld(self:OBBCenter())
 		local Tr = util.TraceLine({
 			start = SelfPos + Vector(0, 0, 50),
@@ -129,33 +135,34 @@ elseif (SERVER) then
 		if not IsValid(self.Pod) then self:CreatePod() end
 		if (Alt) then
 			if (self.State == STATE_UNROLLED) then
-				self:RollUp()
+				if IsValid(self.EZowner) and self.EZowner ~= ply then
+					JMod.Hint(ply,"sleeping bag someone else")
+				else
+					self:RollUp()
+				end
 			elseif (self.State == STATE_ROLLED) then
 				self:UnRoll()
 			end
 		else
 			if (self.State == STATE_UNROLLED) then
-				if IsValid(self.EZowner) and IsValid(ply.JModSpawnPointEntity) and (ply.JModSpawnPointEntity == self) then
+				if not IsValid(self.EZowner) then
+					JMod.SetEZowner(self, ply, true)
+				else
 					if (ply ~= self.EZowner) then
 						JMod.Hint(ply,"sleeping bag someone else")
-					else
-						if not IsValid(self.Pod:GetDriver()) then -- Get inside if already yours
-							self.Pod.EZvehicleEjectPos = self.Pod:WorldToLocal(ply:GetPos())
-							self.Pod:Fire("EnterVehicle", "nil", 0, ply, ply)
-							sound.Play("snd_jack_clothequip.ogg", self:GetPos(), 65, math.random(90, 110))
+					elseif not IsValid(self.Pod:GetDriver()) then -- Get inside if already yours
+						if (IsValid(ply.JModSpawnPointEntity)) then 
+							JMod.SetEZowner(ply.JModSpawnPointEntity, nil) 
+							ply.JModSpawnPointEntity:SetColor(Color(100,100,100))
 						end
+						ply.JModSpawnPointEntity = self
+						local Col = ply:GetPlayerColor()
+						self:SetColor(Color(255*Col.x,255*Col.y,255*Col.z))
+						JMod.Hint(ply, "sleeping bag set spawn")
+						self.Pod.EZvehicleEjectPos = self.Pod:WorldToLocal(ply:GetPos())
+						self.Pod:Fire("EnterVehicle", "nil", 0, ply, ply)
+						sound.Play("snd_jack_clothequip.ogg", self:GetPos(), 65, math.random(90, 110))
 					end
-				else
-					if (IsValid(ply.JModSpawnPointEntity)) then 
-						JMod.SetEZowner(ply.JModSpawnPointEntity, nil) 
-						ply.JModSpawnPointEntity:SetColor(Color(100,100,100))
-					end
-					JMod.Hint(ply, "sleeping bag set spawn")
-					JMod.SetEZowner(self, ply)
-					ply.JModSpawnPointEntity = self
-					local Col = ply:GetPlayerColor()
-					self:SetColor(Color(255*Col.x,255*Col.y,255*Col.z))
-					--JMod.Colorify(self)
 				end
 			elseif (self.State == STATE_ROLLED) then
 				JMod.Hint(ply, "sleeping bag unroll first")
@@ -185,5 +192,24 @@ elseif (SERVER) then
 				self.Pod:Remove()
 			end
 		end
+	end
+
+	function ENT:PostEntityPaste(ply, Ent, CreatedEntities)
+		if IsValid(ply) then
+			JMod.SetEZowner(self, ply, true)
+		elseif self.EZownerID then
+			JMod.SetEZowner(self, player.GetBySteamID64(self.EZownerID), true)
+		end
+
+		if self.State == STATE_ROLLED then
+			self:RollUp()
+		else
+			self:UnRoll()
+		end
+	end
+
+elseif CLIENT then
+	function ENT:Draw()
+		self:DrawModel()
 	end
 end
