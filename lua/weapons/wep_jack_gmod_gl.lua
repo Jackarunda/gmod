67d7bbdc2c71bdbf22e4a2147f7631f70b13bb44
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2025-01-14 17:26:28 -0800

    Fix missing reload msg spam

--- lua/weapons/wep_jack_gmod_gl.lua
+++ lua/weapons/wep_jack_gmod_gl.lua
@@ -5,17 +5,21 @@ SWEP.Slot = 4
 SWEP.ViewModel = "models/weapons/v_jmod_m79.mdl"
 SWEP.WorldModel = "models/weapons/w_jmod_m79.mdl"
 SWEP.ViewModelFOV = 75
+--
 SWEP.BodyHolsterSlot = "back"
 SWEP.BodyHolsterAng = Angle(0, -105, 0)
 SWEP.BodyHolsterAngL = Angle(0, -75, 160)
 SWEP.BodyHolsterPos = Vector(1, -14, -11)
 SWEP.BodyHolsterPosL = Vector(-2.5, -15, 11)
 SWEP.BodyHolsterScale = .9
+--
 JMod.ApplyAmmoSpecs(SWEP, "40mm Grenade")
 SWEP.DamageRand = .1
 SWEP.ShootEntity = "ent_jack_gmod_ezprojectilenade"
 SWEP.MuzzleVelocity = 3000
+--
 SWEP.Primary.ClipSize = 1 -- DefaultClip is automatically set.
+SWEP.ChamberSize = 0 -- sigh
 SWEP.Recoil = 2
 SWEP.Delay = 60 / 100 -- 60/RPM.
 
@@ -51,11 +55,44 @@ SWEP.IronSightStruct = {
 	SwitchFromSound = JMod.GunHandlingSounds.aim.out
 }
 
+--[[SWEP.ExtraIrons = {
+	[1] = {
+		Pos = Vector(-3.15, 0, .05),
+		Ang = Angle(5, 0, -2),
+		Magnification = 1.1,
+		SwitchToSound = JMod.GunHandlingSounds.aim.inn,
+		SwitchFromSound = JMod.GunHandlingSounds.aim.out
+	},
+	[2] = {
+		Pos = Vector(-3.15, 0, -3.75),
+		Ang = Angle(13, 0, -2),
+		Magnification = 1.1,
+		SwitchToSound = JMod.GunHandlingSounds.aim.inn,
+		SwitchFromSound = JMod.GunHandlingSounds.aim.out
+	}
+}--]]
+
 SWEP.ActivePos = Vector(1, 0, 0)
 SWEP.ActiveAng = Angle(1.8, 1.5, -2.5)
 SWEP.HolsterPos = Vector(6, -1, -4)
 SWEP.HolsterAng = Angle(0, 50, 0)
 SWEP.BarrelLength = 30
+
+--[[SWEP.Attachments = {
+	{
+		PrintName = "Optic",
+		DefaultAttName = "Iron Sights",
+		Slot = {"ez_optic"},
+		Bone = "j_front_end_reload",
+		Offset = {
+			vang = Angle(0, 0, 0),
+			vpos = Vector(0, 0, 2.6),
+			wpos = Vector(10, .8, -6),
+			wang = Angle(-10.393, 0, 180)
+		},
+		Installed = "optic_jack_scope_acog"
+	}
+}--]]
 
 --[[
 idle
@@ -90,6 +127,61 @@ SWEP.Animations = {
 		TPAnim = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
 	},
 	["reload_empty"] = {
+		Source = "reload_tac",
+		Time = 4,
+		TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+		Checkpoints = {24, 42, 59, 71, 89},
+		FrameRate = 37,
+		Mult = .95,
+		LHIK = true,
+		LHIKIn = 0.5,
+		LHIKOut = 0.5,
+		ShellEjectAt = 1.8,
+		SoundTable = {
+			{
+				s = "snds_jack_gmod/ez_weapons/gl/move.ogg",
+				t = .3,
+				v = 60
+			},
+			{
+				s = "snds_jack_gmod/ez_weapons/gl/open.ogg",
+				t = .05,
+				v = 65
+			},
+			{
+				s = "snds_jack_gmod/ez_weapons/gl/out.ogg",
+				t = 1,
+				v = 65
+			},
+			{
+				s = JMod.GunHandlingSounds.cloth.magpull,
+				t = 1.7,
+				v = 60,
+				p = 120
+			},
+			{
+				s = "snds_jack_gmod/ez_weapons/gl/in.ogg",
+				t = 2.3,
+				v = 65
+			},
+			{
+				s = JMod.GunHandlingSounds.grab,
+				t = 2.9,
+				v = 60
+			},
+			{
+				s = "snds_jack_gmod/ez_weapons/gl/close.ogg",
+				t = 3.25,
+				v = 65
+			},
+			{
+				s = "snds_jack_gmod/ez_weapons/gl/move.ogg",
+				t = 3.3,
+				v = 60
+			}
+		}
+	},
+	["reload"] = {
 		Source = "reload_tac",
 		Time = 4,
 		TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
