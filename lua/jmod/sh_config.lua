ead9e1d9d742f1fd50623dfc010628f7dbb8c894
Jackarunda <jackarunda@protonmail.com>
2025-02-22 16:54:06 -0700

    config version

--- lua/jmod/sh_config.lua
+++ lua/jmod/sh_config.lua
@@ -13,7 +13,7 @@ function JMod.InitGlobalConfig(forceNew, configToApply)
 		Note = "radio packages must have all lower-case names, see http://wiki.garrysmod.com/page/Enums/IN for key numbers",
 		Info = {
 			Author = "Jackarunda & Friends",
-			Version = 54
+			Version = 55
 		},
 		General = {
 			Hints = true,
