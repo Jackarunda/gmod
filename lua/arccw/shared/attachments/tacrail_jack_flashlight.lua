6cacb40cfad40b728abd07952f5e929a3df917c0
AdventureBootCoder <96029388+AdventureBootCoder@users.noreply.github.com>
2026-04-22 23:39:05 -0700

    add tactical flashlight as an attachment

---
+++ lua/arccw/shared/attachments/tacrail_jack_flashlight.lua
@@ -0,0 +1,16 @@
+﻿att.PrintName = "EZ Tactical Flashlight"
+att.Icon = Material("entities/acwatt_tac_pointer.png")
+att.Description = "A powerful precision flashlight for your weapon"
+att.Desc_Pros = {}
+
+att.Desc_Cons = {"- Visible light"}
+
+att.AutoStats = true
+att.Slot = "ez_tac_rail"
+att.Model = "models/weapons/arccw/atts/laser_pointer.mdl"
+att.FlashlightFOV = 45
+att.Flashlight = true
+att.FlashlightBone = "laser"
+att.FlashlightColor = Color(255, 255, 255)
+att.FlashlightTexture = "engine/lightsprite"
+att.FlashlightBrightness = 1
